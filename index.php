61b8d89cbf1d5ac04001ab4c6f20578f54d44cbd
jim <jbwhite2@ncsu.edu>
2021-12-18 18:29:46 -0500

    img

--- index.php
+++ index.php
@@ -4,8 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-    <meta name="generator" content="Hugo 0.88.1">
     <title>Jim White&lsquo;s Homepage</title>
 
 
@@ -17,9 +15,6 @@
     <style>
     </style>
 
-
-    <!-- Custom styles for this template -->
-    <!-- <link href="navbar-top-fixed.css" rel="stylesheet"> -->
   </head>
   <body>
 
