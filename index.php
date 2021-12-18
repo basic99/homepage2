64cfae88d24e8199957803d7fe0f9b6a43d643c1
jim <jbwhite2@ncsu.edu>
2021-12-18 18:54:16 -0500

    menu

--- index.php
+++ index.php
@@ -26,11 +26,17 @@ include "menu.php";
   <img src='img/mars_wp_hdr.png' />
 </div>
 <main class="container">
-
-  <div class="bg-light p-5 rounded">
+<div class="row g-5">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
     <h1>Navbar example</h1>
     <p class="lead">This example is a quick exercise to illustrate how fixed to top navbar works. As you scroll, it will remain fixed to the top of your browser’s viewport.</p>
     <a class="btn btn-lg btn-primary" href="../components/navbar/" role="button">View navbar docs &raquo;</a>
+  </div>
+  </div>
+
+  <div class="bg-light p-5 rounded">
+
   </div>
 </main>
 
