2dfec0d42f68ccba351daf7aa4473cb1e0bb0d5e
jim <jbwhite2@ncsu.edu>
2021-12-21 20:55:58 -0500

    article

--- article/notes-on-james-mcdaniel-white.php
+++ article/notes-on-james-mcdaniel-white.php
@@ -29,7 +29,30 @@ include "../menu.php";
 <div class="row g-5 bg-light rounded">
   <div class="col-md-2"></div>
   <div class="col-md-8">
-    <h2>Home</h2>
+    <h2>Notes On James Mcdaniel White</h2>
+
+    <p>
+James McDaniel White was born at the White home place on Rocky River in Chatham County, six miles from Pittsboro on May 18, 1836. This place had been bought in 1785 by his great grandfather, John White, who came from Virginia. His father, William White, was a member of Mays Chapel, a deacon, and took an active part in Baptist church work.
+</p>
+<p>
+James McDaniel White was named for a noted Baptist preacher of that day, James McDaniel, and was called "Mac" by his contemporaries.
+</p>
+<p>
+He was graduated from Wake Forest College in 1859 with the degree of Master of Arts. That summer while riding to Pittsboro, he stopped at a creek to water his horse. At the same time an acquaintance coming from the other direction did the same thing and the two engaged in a conversation. In the course of this White, speaking of his plans, expressed the desire to teach rather than farm. His companion remarked that he had just heard of a good opening in Clayton, in Johnston County, and gave him names of persons to whom he could write. The result was that the fall of 1859 found White in Clayton as principal of its academy.
+</p>
+<p>In politics he was a Whig as his father, a strong Union man and opposed to anything like secession.
+<p>
+In less than two years the question of secession was submitted to the voters of the state. White threw himself wholeheartedly into this and engaged in a joint debate through Johnston County opposing secession. Incidentally his opponent in the debate secured an office job in Raleigh for the duration of the war. In this election secession was defeated, but only a short time afterwards, in April 1861.
+</p>
+<h2>Capt. James McDaniel White</h2>
+
+<h4>Captain Third North Carolina Cavalry</h4>
+<p>
+Made Captain March 1865 and assigned to duty on Gen. Barringers staff, but was wounded April 1st Battle of Five Forks and sent to the hospital at Danville. Lee surrendered April 9th and Capt. White left hospital with a companion Pvt. May to avoid surrendering. Rode in a box car to Greensboro. In some way they acquired a horse in Greensboro and May held Capt. White on his horse until he got to his house in Chatham County. He was wounded through the body.
+</p>
+<p>
+There was a picture of Capt. White in his uniform, but we have never been able to find it. The last time Bruce White saw it was when he was in his house in Clayton when he was a boy.
+</p>
 
 
 
