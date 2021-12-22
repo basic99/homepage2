b20f54d60b854c2c54e6ced212f47c9e45a737c1
jim <jbwhite2@ncsu.edu>
2021-12-21 20:47:40 -0500

    article

--- article/essex-manor-records.php
+++ article/essex-manor-records.php
@@ -31,43 +31,44 @@ include "../menu.php";
   <div class="col-md-8">
     <h2>Essex Manor Records</h2>
 
-Records of the Manors of Willingale Doe and Shelley in Essex translated from Latin by Diana Spelman dianaspelman@waitrose.com
+<p>Records of the Manors of Willingale Doe and Shelley in Essex translated from Latin by Diana Spelman dianaspelman@waitrose.com
 
-[marginal heading] Willyngale Doo
+<p>[marginal heading] Willyngale Doo
 
-At the court held there on Tuesday next after the feast of the Translation of St Thomas the Martyr in the thirteenth year of the reign of King Henry the Seventh [10 July 1498], it was enrolled thus:
+<p>At the court held there on Tuesday next after the feast of the Translation of St Thomas the Martyr in the thirteenth year of the reign of King Henry the Seventh [10 July 1498], it was enrolled thus:
 
-To this court came John Smyth of Hoton [Hutton] and surrendered in the full court all his  customary tenement with lands belonging to the same tenement called Newmans into the hand of the lord, for him &amp; his heirs, to the use of Robert White &amp; Joan the daughter of the same Robert &amp; Joan lately his wife &amp; formerly the wife of Thomas Smyth,  to which same Robert White &amp; Joan his daughter the lord, by his steward,  has granted seisin out of his hands by the virge, to hold to him and the heirs &amp; assign of the aforesaid Joan, at the will of the lord, according to the customs of the manor, for all ancient services owing for it.  But under this following condition, that if the aforesaid Robert White should pay or make payment to the aforesaid John Smyth or his true attorneys, executors or assigns 23 shillings 4 pence in the following form, namely at the feast of MIchaelmas next coming after the date of this court 6s 8d, and at the next court held there 6s 8d, and at the next court to be held there in the future after that 10s, then the present surrender should remain in force.  And if default is made in any payment of the aforesaid payments in part or in full, against the form beforewritten, that then it will be lawful for the said John Smyth, his heirs &amp; assigns, by the licence of the lord &amp; the court to re-enter the whole of the aforesaid tenement &amp; premises, to possess &amp; hold it as in his former estate, notwithstanding in any way this surrender.  And he gave the lord for a fine for the whole estate, to hold as appears in the court roll, &amp; he made fealty for it to the lord &amp; he was admitted tenant, holding of the lord in the form aforesaid.
+<p>To this court came John Smyth of Hoton [Hutton] and surrendered in the full court all his  customary tenement with lands belonging to the same tenement called Newmans into the hand of the lord, for him &amp; his heirs, to the use of Robert White &amp; Joan the daughter of the same Robert &amp; Joan lately his wife &amp; formerly the wife of Thomas Smyth,  to which same Robert White &amp; Joan his daughter the lord, by his steward,  has granted seisin out of his hands by the virge, to hold to him and the heirs &amp; assign of the aforesaid Joan, at the will of the lord, according to the customs of the manor, for all ancient services owing for it.  But under this following condition, that if the aforesaid Robert White should pay or make payment to the aforesaid John Smyth or his true attorneys, executors or assigns 23 shillings 4 pence in the following form, namely at the feast of MIchaelmas next coming after the date of this court 6s 8d, and at the next court held there 6s 8d, and at the next court to be held there in the future after that 10s, then the present surrender should remain in force.  And if default is made in any payment of the aforesaid payments in part or in full, against the form beforewritten, that then it will be lawful for the said John Smyth, his heirs &amp; assigns, by the licence of the lord &amp; the court to re-enter the whole of the aforesaid tenement &amp; premises, to possess &amp; hold it as in his former estate, notwithstanding in any way this surrender.  And he gave the lord for a fine for the whole estate, to hold as appears in the court roll, &amp; he made fealty for it to the lord &amp; he was admitted tenant, holding of the lord in the form aforesaid.
 
-#################################################
+  <hr style="height:4px;border-width:0;color:gray;background-color:gray">
 
-Wyllynghale Doo
 
-[Latin] On the roll of view of frankpledge with court held Tuesday 10th September 24 Henry VIII [1532] among other things it is recorded that
+<p>Wyllynghale Doo
 
-At this court come Thomas Barnarde &amp; Margaret his wife in person (she being examined in secret) and surrender into the hands of the lord the reversion of one tenement with customary land belonging to the same, with appurtenances called Newmans, and of one tenement with 12 acres of land called Gowres to the use of Richard Barnard, son of Thomas &amp; Margaret.  To whom is granted seisin, to hold to him &amp; his assigns at the will of the lord, for rent &amp; services &amp;c under certain conditions specified in the indenture made between Thomas, Margaret &amp; Richard.  Richard should pay fines on the deaths of Thomas and Margaret &amp;c
+<p>[Latin] On the roll of view of frankpledge with court held Tuesday 10th September 24 Henry VIII [1532] among other things it is recorded that
 
-###############################################
+<p>At this court come Thomas Barnarde &amp; Margaret his wife in person (she being examined in secret) and surrender into the hands of the lord the reversion of one tenement with customary land belonging to the same, with appurtenances called Newmans, and of one tenement with 12 acres of land called Gowres to the use of Richard Barnard, son of Thomas &amp; Margaret.  To whom is granted seisin, to hold to him &amp; his assigns at the will of the lord, for rent &amp; services &amp;c under certain conditions specified in the indenture made between Thomas, Margaret &amp; Richard.  Richard should pay fines on the deaths of Thomas and Margaret &amp;c
 
-[Latin] To all the faithful in Christ &amp;c Richard Sampford of Shellow Bowelles in Essex Yoman, greetings.  Let it be known that I, Richard Sampford, according to a grant &amp; agreement dated 2nd January last past between me and a certain Richard Bernard of Roding Bernars in Essex yoman, have granted &amp; confirmed by this present indenture two messuages or tenements called Newmans &amp; Gowers with all gardens appertaining to the same, and also 7 crofts of land containing about 30 acres, and two meadows containing about 2 acres, now enclosed, in Wyllinghale Doo in Essex, now or lately in the occupation of the said Richard Bernard or his assigns, to have &amp; to hold to him, his heirs &amp; assigns forever, of the chief lord of that fee, for service &amp; annual rent of 4d, of my manor of Waltons Hall.  If the rent falls into arrears, then I or my heirs or the owner of the manor of Waltons Hall, may distrain for the arrears due from Richard Bernard or his heirs upon the property.  In witness whereof I have put my seal to each part of the indenture.   5th January 5 Elizabeth I [1563]  Signed Rychard Sampford   witnessed by William Lynese clarke, Rychard Wethear, William Pole, Thomas Fox, John Willes &amp; Thomas Sampford
+  <hr style="height:4px;border-width:0;color:gray;background-color:gray">
 
-[dorse]  "Possession &amp; seacon herof dellyvered by the within named Rychard Sampford to the within named Rychard Barnard &amp; his heyers in the presences of John Dyer, Thomas Sampford, Rychard Wethar, William Pole &amp; Thomas Ponde with othar the daye &amp; yere within wrytten    5 January 5 Eliz:"
 
-<a id="shelley"></a>
+<p>[Latin] To all the faithful in Christ &amp;c Richard Sampford of Shellow Bowelles in Essex Yoman, greetings.  Let it be known that I, Richard Sampford, according to a grant &amp; agreement dated 2nd January last past between me and a certain Richard Bernard of Roding Bernars in Essex yoman, have granted &amp; confirmed by this present indenture two messuages or tenements called Newmans &amp; Gowers with all gardens appertaining to the same, and also 7 crofts of land containing about 30 acres, and two meadows containing about 2 acres, now enclosed, in Wyllinghale Doo in Essex, now or lately in the occupation of the said Richard Bernard or his assigns, to have &amp; to hold to him, his heirs &amp; assigns forever, of the chief lord of that fee, for service &amp; annual rent of 4d, of my manor of Waltons Hall.  If the rent falls into arrears, then I or my heirs or the owner of the manor of Waltons Hall, may distrain for the arrears due from Richard Bernard or his heirs upon the property.  In witness whereof I have put my seal to each part of the indenture.   5th January 5 Elizabeth I [1563]  Signed Rychard Sampford   witnessed by William Lynese clarke, Rychard Wethear, William Pole, Thomas Fox, John Willes &amp; Thomas Sampford
 
-################################################
+<p>[dorse]  "Possession &amp; seacon herof dellyvered by the within named Rychard Sampford to the within named Rychard Barnard &amp; his heyers in the presences of John Dyer, Thomas Sampford, Rychard Wethar, William Pole &amp; Thomas Ponde with othar the daye &amp; yere within wrytten    5 January 5 Eliz:"
 
-Shellegh   View of Frankpledge with court held Wednesday after Trinity 3 Henry VIII [1511]
+  <hr style="height:4px;border-width:0;color:gray;background-color:gray">
 
-[Latin] And that Totherus White &amp; John Luke are common bakers in several bakeries of the lord without the lord's licence, taking &amp; carrying away bread &amp;c.  Fined 20d each.
 
-[Latin] And that Tocherus White &amp; John Luke are common breakers of fences of the lord &amp; his tenants there, to the damage &amp;c.  They are fined 2d each.
+<p>Shellegh   View of Frankpledge with court held Wednesday after Trinity 3 Henry VIII [1511]
 
-###############################################
+<p>[Latin] And that Totherus White &amp; John Luke are common bakers in several bakeries of the lord without the lord's licence, taking &amp; carrying away bread &amp;c.  Fined 20d each.
 
-Shellegh View of Frankpledge with court  ….. St Margaret  16 Henry VIII [1524]
+<p>[Latin] And that Tocherus White &amp; John Luke are common breakers of fences of the lord &amp; his tenants there, to the damage &amp;c.  They are fined 2d each.
+  <hr style="height:4px;border-width:0;color:gray;background-color:gray">
 
-[Latin] It is ordered that Humphrey Whyte [with six others also amerced] is amerced 2d for residing within the manor &amp; defaulting [not attending] at this court.
+
+<p>Shellegh View of Frankpledge with court  ….. St Margaret  16 Henry VIII [1524]
+
+<p>[Latin] It is ordered that Humphrey Whyte [with six others also amerced] is amerced 2d for residing within the manor &amp; defaulting [not attending] at this court.
 
 
 
