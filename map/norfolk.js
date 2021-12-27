4996c70e9327022942cc20b88e1ffc97bfed5fb4
jim <jbwhite2@ncsu.edu>
2021-12-26 21:29:51 -0500

    dev map

--- map/norfolk.js
+++ map/norfolk.js
@@ -1,9 +1,12 @@
 // https://docs.mapbox.com/help/tutorials/add-points-pt-2/
+// https://docs.mapbox.com/mapbox-gl-js/example/geojson-line/
+// https://docs.mapbox.com/mapbox-gl-js/example/geojson-layer-in-stack/
+// https://www.lostcreekdesigns.co/writing/a-complete-guide-to-sources-and-layers-in-react-and-mapbox-gl-js/
 
 mapboxgl.accessToken = 'pk.eyJ1Ijoiam1zYnJjd2h0IiwiYSI6ImNreGkzam9lOTBnbDYyb3F3cjF0a3VmZWIifQ.oWTLtlwjajr6o1jSvVIZRQ';
 var map = new mapboxgl.Map({
   container: 'map',
-  style: 'mapbox://styles/jmsbrcwht/ckxns9ktj3rio14s6dupchz22',
+  style: 'mapbox://styles/jmsbrcwht/ckxo1t2ya4nj315t8lrnv9ifa',
   center: [-76.15, 36.8],
   zoom: 8
 });
@@ -13,13 +16,21 @@ map.on('load', () => {
     'url': 'mapbox://jmsbrcwht.9fmtj111'
   });
 
+  map.addSource("metes_bounds", {
+    type: "vector",
+    url: "mapbox://jmsbrcwht.ckxnjod974bav27ood55nbuyd-41oks",
+  })
 
   map.addLayer({
     'id': 'usgs_1907',
     'source': 'usgs_1907',
     'type': 'raster'
   });
-
+  map.addLayer({
+    'id': 'metes_bounds',
+    'source': 'metes_bounds',
+    'type': 'line'
+  });
 
 });
 
