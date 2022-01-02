fb22c8441fa2a0c36a1e38abd2adb9f7d0012b79
jim <jbwhite2@ncsu.edu>
2022-01-02 11:40:17 -0500

    article

---
+++ article/pierce-southampton-records.php
@@ -0,0 +1,99 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+    <h2>Pierce Southampton Records</h2>
+    Southampton Deed Book 4 1767-1773, p.56
+
+This indenture made the twenty fourth day of Dec. in the year of our lord one thousand/ seven hundred &amp; sixty seven between Arthur Arrington of Edgebombe County of North Caroling of the one/ part &amp; Rice Bolton Pierce of Southampton county of the other part witneseth that the sd/ Arthur Arrington for in consideration of the sum of seventy pounds to him in hand/ paid at or before the ensealing &amp; delivery of these presents the receipt whereof he doth/ hereby acknowledge hath granted bargained sold aliened enfeoffed &amp; confirmed &amp; by these/ presents doth grant bargain &amp; sell &amp; confirm unto the sd Rice B Pierce &amp; his heirs/ forever one certain tract or parcel of land situated lying and being in the count of So/ hampton containing two hundred &amp; eighty two acres be more or less bounded as/ followeth [metes and bounds, mentions Little cove branch] being part of a tract of land containing four hundred &amp; eighty acres/ which was made over to the said Arrington in Southampton court bearing date one thousand/ seven hundred &amp; sixty six recourse being thereto had will more at large appear to / have and to hold ...
+
+Signed sealed &amp; delivered
+in presence of
+Thomas Chaperel
+James Atkinson
+John Adams
+
+&nbsp;
+
+Southampton Will Book 4 1783 - 1797, p. 353
+
+In the name of God amen January 10 day 1781 I Elias/ Williams of Southampton County being now in perfect health and/ sound memory thanks be to God and being mindful of the mortality/ of the body and not knowing how soon it may please God to call/ me I
+make and ordain this my last willl and testament as/ followeth (to wit)
+Impr I give and bequeath unto my nephew Matthew Pierce my black/ Mare called Ravin to him and his heirs
+Item I give and bequeath unto my nephew Nicholas Willimas my/ sorrell colt that is now at swift creek to him
+Item I give and bequeath unto my nephew Elias Willimas my old/ mare called Flower to him
+Item I give and bequeath my stock of cattle hogs and sheep to be equally divided between all my brothers and sisters. I likewise give them all my household furniture of every kind to be/ equally divided between them
+Item I give and bequeath all my wearing apparel unto my/ brother John Williams
+Lastly I constitute and appoint my friend Rice B. Pierce/ to be my whole and sole executor of this my last/ will and testament ratifying and confirming this/ and no other to be my last will and testament
+
+In witness where of I have set my hand and fixed my seal the/ day and date first above written
+Signed and Sealed in
+presence of
+Nicholas Williams
+Benjamin Williams
+
+Southampton Court Minute Book 1793-1799, p. 12
+Court 9 May 1793
+
+Pierce's Heirs vs Pierce's widow ... leave to amend the bill/ by striking out the name of Matthew Pierce &amp; it is further/ deemed and ordered that the commissioners formerly appointed make/ equal partition &amp; division of the remaining lands &amp; slaves whereof/ R. B. Pierce died seized &amp; possessed (after setting apart the dower/ of the deft) between the surviving complainants &amp; report.
+
+Southampton Will bk 4, p.644
+
+In the name of God Amen I Elizabeth Peirce of the Parish of Nottaway &amp;/ county of Southampton being low in health but of sound mind &amp; disposing meaning do make &amp;/ ordain this my last will &amp; testment in manner &amp; form following _ First I give to my/ daughter Elizabeth Peirce one bed &amp; furniture to be chosen by her from amongst all my/ beds to her &amp; her heirs forever _ Secondly I give to my daughters Martha Cobb and/ Elizabeth Peirce all my cupboard furniture to be equally divided between them _/ Thirdly It is my will &amp; desire that all my estate of what kind or nature soever/ to be sold &amp; the money arising from the sale after my just debts being paid to be/ divided as follows one fourth part to my son Peter Peirce one fourth part to my son/ Bolton Peirce &amp; the other half to be equally divided betwnn other children Spencer/ Peirce Nathaniel Peirce Martha Cobb &amp; Elizabeth to them &amp; their heirs forever Lastly/ I do nominate &amp; appoint Robert Goodwyn &amp; Joseph Vick Exors of this my last will &amp;/ testament &amp; I do revoke &amp; make void all other ... of wills by me made declare/ this to be my last will &amp; testament In witness whereof I have hereunto set my hand/ &amp; seal this 28th day of April 1794.
+Signed Sealed Published
+&amp; declared in presence of
+Josiah Cashiele
+Ja. Vick
+Benj. Andrews
+At a court held for the county of Southampton the 9th day of Oct 1794 this will/ was presented in court by Rich. Goodwyn one of the Exors therein named the other/ refusing proved by the oaths of Cashiel &amp; Jo Vick two of the witnesses thereto &amp;/ ordered to be recorded &amp; on the motion of the sd Exor oathe made according to lawe .../ is granted him for obtaining a probate thereof in due forme giving security whence from.
+Southampon Will bk 9, p.68
+
+The estate of Mrs Elizabeth Pierce
+In account with Robert Granger Admin
+---------------------------------------
+various accounts
+--------------------------------------
+The Legatees entitled to this sum are Spencer Pierce Nathaniel Pierce/ Rice B Pierce Peter Pierce and the orphans of Martha Cobb late/ Pierce and the orphans of Elizabeth Lewis formerly Pierce making / in the whole sixe legatees which will give each legatee the sum / of one Hundred and Eighty one dollars this 11th day of / Dec 1819
+
+Southampton Will bk 9,p. 157
+
+We the undersigned arbitrators mutually chosen by Rice B Pierce Peter Pierce/ and Nathaniel Pierce heirs and legates of Elizabeth Pierce decd widow/ and relict of Rice B Pierce decd of the one part and Benjamin Cobb/ administrator of Robert Granger decd who was the administrator of the/ said Elizabeth Pierce decd of the other part to settle adjust and finally/ determine all matters in difference between the parties on relation to the/ administration of the said Robert Granger on the estate of the said/ Elizabeth Pierce decd and whereas for the putting an end to the/ said difference and dispute that the Rice B Pierce &amp; Nathaniel/ Pierce and Benjamin Cobb by their several loans or obligations bear/ ing data the ___ day of ___ 1819 are reciprocally bound each to / the other in the sum of ___ to stand to and abide/ the final determination of us Coleen Ketchew John Jeffries and Jeremiah/ Cobb arbitrators indifferently chosen between the said parties have/ this day taking the mattters and things in difference between the said / parties under our consideration and having had due deliberation thereon the/ testi ony of the parties having been submitted to us do award and determin as folows/ that the said Bnejamin Cobb admin of Robt Granger dect pay to the said Rice B/ Pierce and Nathaniel Pierce their proportion of the estate of the said Elizabeth/ Pierce decd amounting to the some of one hundred and eighty one dollars/ each by reference to the account it will appear fully with interest/ Therein from december 30 1819 the paid given under our hand and sealed the / 21st day of Feby 1820
+
+
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
