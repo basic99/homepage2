527f6209e5d9c08bfc025c893bf802acd16dd0af
jim <jbwhite2@ncsu.edu>
2022-01-02 16:07:37 -0500

    article

---
+++ article/southampton-county-chancery-1794.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+    <h2>Home</h2>
+    1794-020
+Southampton County Chancery Papers
+Pierce's Heirs vs Pierce, Widow 9/1794
+
+<img class="alignnone" src="http://jimserver.net/graphics/171246.png" alt="" width="1076" height="792" />
+
+To the worshipful the court of Southampton county in chancery/ sitting, Humbly complaining sheweth unto your worships your/ orators Michael Cobb and Martha his wife, (Spencer Pierce/ (Matthew Pierce), ^Betsy Pierce, Nathaniel Pierce, Peter Pierce, &amp; Bolton / Pierce by Samuel Calvert -- their guardian &amp; near/ friend appointed by this court for the express purpose of carrying / into effect the premises) that sometime on or about the / ___ day of ___ in the year of our Lord 1792 a certain Rice Bolton Pierce of this county departed this life intestate/ being seised &amp; possessed at the time of his death of a considerable/ estate both real &amp; personal - Your orators further shew/ that they are the legal heirs &amp; representatives of the decd/ &amp; that they have often applied to Elizabeth Pierce the widow / &amp; relict of the sd decedent to have her thirds in the lands &amp;/ slaves of her sd. husband set apart &amp; alotted her, in order that/ your orators might have a fair &amp; equal distribution of/ the residues between them. - But so it may / it please your worships that the sd. Elizabeth Pierce the / widow &amp; relict of the sd. Rice Bolton Pierce [torn]/ and considering with divers other (whose names when lik [torn]/ ye orators pray may be made defendant to this their bill with of [torn]/ to charge them) hitherto hath refused to comply with [torn]/ ye orators most reasonable request; pretending [torn]/ out in ... that your oratores are not entitled to [torn]/ part of the lands &amp; slaves of her late husband will [torn]/ decease -- In tender consideration whereof, &amp; [torn]/ much ... orators are properly relievable in a court [torn]/ equity ... the and therefore that the sd. Elizabeth / may ... oath full true &amp; perfect and [torn]/
+
+<img class="alignnone" src="http://jimserver.net/graphics/171247.png" alt="" width="1076" height="792" />
+
+make to all &amp; singular the allegations of this bill &amp; as fully as/ if the same were repeated &amp; interrogated, but more especially/ whether ye orators are not the legal heirs &amp; representatives of / the sd. Rice Bolten Pierce , did he not die seised &amp; possesed/ of a considerable estate both real and personal &amp; that she/ not refused to take or accept her thirds. had that his / worshipful court may order &amp; secure some fit &amp; able/ persons to alot &amp; set apart her thirds in the lands &amp; slaves/ of which her sd. husband died seized &amp; possessed, &amp; the residue/ of the sd. lands &amp; slaves to be equally divided among your orators or make any further or other decree as my seem/ just &amp; right, is the prayer of sd. orators, &amp; they will/ ever pray &amp;c ---
+
+<img class="alignnone" src="http://jimserver.net/graphics/171248.png" alt="" width="1076" height="792" />
+
+The answer of Elizabeth Pierce widow &amp; relict of Rice/ Bolton Pierce decd. to the bill of complaint of Michael/ Cobb and Martha his wife, (Spencer Pierce, Matthew Pierce/ Betsy Pierce, Mathaniel Pierce, Peter Pierce &amp; Bolton/ Pierce, exhibited against her. This defendant now &amp; at all / times hereafter saving &amp; reserving to herself all &amp; all manner / use of benefit &amp; advantage to the many untruths, imperfec=/ tions, &amp; indeficiences in the complainants sd bill .../ complaint contains, for ... thereto, or to ..../ she is advised in material for her to make answer unto/ answeres &amp; says, That true it is she is the widow &amp; relict/ of Rice Bolton Pierce decd., &amp; he died somtime/ on or about the time in complainants bill mentioned/ &amp; that he was seised &amp; possessed of a considerable estate/ both real &amp; personal. - This defendant denies/ all combination or that she has any objection/ to have her thirds in the lands &amp; slaves .../set apart to her.... And if there is any other [torn]/ matter or thing in the complainants sd. bill [torn]/ &amp; not answered or denied the respondent is igno [torn]/ of, &amp; pray to have dismissed with her reaso [torn]/ ... this be half sustained.
+
+<img class="alignnone" src="http://jimserver.net/graphics/171250.png" alt="" width="1076" height="792" />
+
+At a court of quarter session held for the county of Southampton/ the 14th day of March 1794
+In Chancery
+
+Michael Cobb and Martha his wife, &amp; Spencer Pierce/ Elizabeth Pierce Nath. Pierce Peter Pierce &amp; Bolton/ Pierce (which sd. Spencer Elizabeth Nath. Peter &amp; Bolton/ are under the age of 21 years) by Saml. Calvert their guardian
+
+against
+
+Elizabeth Pierce widow &amp; relict of Rice B. Pierce decd.
+
+This cause being docketed by consent of the parties/ was this day heard on the bill of the complainants &amp; answer of the / defendant On consideration whereof it is decreed &amp; ordered/ (Saml. Calvert being first apponted guardian to the complainants who/ are infants) that Thomas Gray Saml. Kello Joseph Vick &amp; / Matthew Calvert or any three of them do allot assign &amp; set apart/ the dower of the defendant Elizabeth in the lands &amp; slaves whereof/ Rice Bolton Pierce died seized &amp; possessed and make an equal division of the remaining land and slaves among the compl./ &amp; make report in due order to a final decree.
+
+<img class="alignnone" src="http://jimserver.net/graphics/171251.png" alt="" width="1076" height="792" />
+
+In obedience to an order of the worshipful court of Southampton/ hereto annexed We the subscribers have alloted and set apart/ the dower of Elizabeth Pierce the widow of Rice B. Pierce decd./ in the slaves of the sd. Rice B. Pierce and divided the remaining / slaves amng the complainants in the following manner
+
+Viz. To Mrs. Pierce her dower (10 slaves) Will Nanny &amp; her child/ Dinah, Jenny, Jacob, Eve, Gaby, Slyvia &amp; Charlotte valued to pds 291..13.4/ &amp; to receive from the complainants pds 1..17.1 1/4 } 293..10.5
+To Spencer Pierce negroes Ned &amp; Milbe valued to pds 91..10/ &amp; to receive pds 6..7.1/2 } 97..17.1/2
+Michael Cobb &amp; Martha his wife Sampson &amp; Mason valued to pds 107..10/ &amp; to pay 9..12.11 1/2 } 97..17.1/2
+Elizabeth Pierce Sam and Bob valued to 87..10/ &amp; to receive 10..7.1/2 } 97..17.1/2
+Nath. Pierce Rachael &amp; Adam valued 90/ &amp; to receive 7..7..1/2 } 97..17.1/2
+Peter Pierce Betty her child Annita &amp; Ned valued to 102..10/ &amp; to pay 4..12.1 1/2 } 97..17.1/2
+Bolton Pierce Easter &amp; Cook valued to 110 / &amp; to pay 12..2.11 1/2 } 97..17.1/2
+
+We further report that after repeated attempts to divide / the lands among the complainants &amp; lay off the dower of / defendant We have not been able to effect owing to the / difficulties atending the diveision &amp; the evidend injustices that/ some of the partis would receive
+Southampton May 1794 Sam Kello, Ja. Vick, Matt. Calvert
+
+<img class="alignnone" src="http://jimserver.net/graphics/171253.png" alt="" width="1076" height="792" />
+
+In obedience to an interlocutary decree passed South-/ ampton August court 1794. We have divided the / land &amp; slaves belonging to the estate of Rice Bolton/ Pierce deceased in the following manner to wit:/ To Bolton Pierce the tract of land known by the name/ of "Calthorpe's" containing 228 acres &amp; negroe Jacob/ &amp; he to receive twenty one pounds &amp; 4d: - to Spencer/ Peirce the tract of land known the name of/ the "Home tract" containing 226 acres &amp; negro Will/ &amp; he pay seventy nine pound nineteen shilings/ &amp; 8d: - To Elizabeth Peirce the tract of land known / by the name "Spivey's" &amp; negros Jimmy &amp; Sylvia/ &amp; she receive sixty four pounds &amp; 4d:- / To Nathanial Pierce the tract of land known by/ the name of "North side of Chappels" containing/ 163 acres ^ two negroes Nanny &amp; Dinah &amp; he pays two pounds two shillings &amp; 8d:/ To Michael Cobb the tract of land known by the name / of "the remainder of Chappels" containing 163 &amp; negro/ Eve &amp; pays two pounds two shilings &amp; 8d: - To/ Peter Pierce the tract of land known by the name/ of "Simmon's tract" containing 162 acres &amp; negros/ Gaby &amp; Charlotte &amp; he to pay fifteen shillings &amp; 8d. Given under our hands this first day of September/ in the year of Christ 1794
+Sam Calvert, Tho. Butts, William Willons
+
+
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
