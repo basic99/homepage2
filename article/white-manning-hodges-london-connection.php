6da85b0d297dcf69272e2972b49a836ed8804ff9
jim <jbwhite2@ncsu.edu>
2022-01-02 22:46:45 -0500

    templates

--- article/white-manning-hodges-london-connection.php
+++ article/white-manning-hodges-london-connection.php
@@ -30,6 +30,85 @@ include "../menu.php";
   <div class="col-md-2"></div>
   <div class="col-md-8">
     <h2>Home</h2>
+    In this article I consider records that show a possibility that Thomas White, John Manning, and Roger Hodges of Lower Norfolk County were immigrants from Westminster or the west end of London. The evidence is admittedly circumstantial. Many of the records that I show as possibly referring to someone in Virginia and England I'm sure refer to different people of the same name, but at least some of them might refer to the same person or at least a relative of the same name. Wills that support this article are another link on this site.
+
+<img class="alignnone" src="http://jimserver.net/graphics/west_london-300x255.png" alt="" width="300" height="255" />
+
+This map shows the location of parishes mentioned. St Martin in the Fields, St Margaret, and Clerkenwell are labelled.  St Clement Danes is #10 and St Andrew Holborn is # 31. St Brides is considered part of the City of London and does not appear on this map but is at the western edge of the city.
+
+I consider these 3 baptism records as possibly being the immigrants to Lower Norfolk:
+
+Thomas White, bapt 5 Feb 1597/8, Saint Martin in the Fields, Westminster
+John Manning S of Robert 13 Feb. 1616/7, Saint Clement Danes, Westminster
+Roger Hodges, s of John and Mary Hodges  09 Feb 1642, St. Bride Fleet St., London
+
+Thomas White deposed 15th July 1657 he was 58 or thereabouts. If his actual date of birth was as above he would have actually been 59 1/2. The passenger list of the Globe sailing from London to Virginia in August 1635 list of passengers lists John Manning's - 20, and Christoper Hamond - 32. If this refers to John Manning of Saint Clements his actual age would have been 18 1/2. Roger Hodges first appears in Lower Norfolk County records in August 1665, and married John Manning's daughter by 1669, so would have been about the right age to be the person baptized at St. Bride.
+
+#################################################################
+
+Thomas White first appeared in Virginia records as a headright for a grant of 250 acres in Lower Norfolk County 10 March 1637/8 to Thomas Sawyer along with other headrights Christopher Hamond, Wm. Spackford, Tho: Kirke, and Isabell Ablesonne. Christopher Hamond must be the same who was listed as a passenger on the Globe. This baptism record is likely the passenger on the Globe and would be 2 years from what is listed on the passenger list:
+
+Christopher Haman s of Thomas Haman, bapt. Feb. 1600/1, Saint Margaret, Westminster
+
+The other name on the headright list of interest is Wm Spackford. Sparchford is a relatively unusual name and there are several entries with various spellings for this name in the Saint Martin in the Fields parish register around 1600. One of which is the baptism and shortly thereafter burial of Guilielmus Sparcksford/Sparcheforde in 1596. Mostly it is spelled Sparchford, but the spelling on the headright is close enough to think that that person might have been related to the Sparchfords of Westminster. Also of interest is the will of Robert Sparchford of Saint Martin dated 1591 that mentions 'John White of the parish of St. Clementes Danes with oute Temple bare in the countie of Midd Tailor.' This appears to show a connection of the Sparchford and White families in London.
+
+Records in Lower Norfolk County in 1653 mention a Raffe White. Also, in Lower Norfolk County court 20 March 1656/7 a record with Thomas White was followed by this record:
+
+Whearas Mr Edmund Bowman, atturney of Mr. Ralfe White of London ... against the estate of Rich: Sternell decd for six hh tobacco &amp; caske one hh whereof being paid by Robt. Causby and the court being informed ....(Brayton, Lower Norfolk book D, f. 68)
+
+Sometimes in county court consecutive records show some type of connection, so possibly Thomas White was related to Ralph White of London. It appears he was in Virginia for a time around 1653 and returned to England by 1656.
+
+Parish records of Saint Martin in the Fields show marriage of Radulph White &amp; Alicam White, 12 May 1617, followed by the births of 11 children from 1618 to 1635, including a Radulfus White, bapt. 7 Apr 1618. A search for a will of Ralph White was made and several were found, but none that appeared to be Ralph White of Saint Martins.
+
+Parish records of St Martin in the Fields concerning a Thomas White:
+
+Thomas White, bapt 5 Feb 1597/8
+Mr. Tho. White, burial 18 Feb 1601/2 occisus
+Thomas White, burial 13 Nov 1615
+Thomas White &amp; Jana Chamberlaine, mar. 13 Oct. 1624
+Thomas White a man, bur. 19 Oct. 1625
+
+and other records concerning a Thomas White of St Martin in the Fields:
+
+Thomas White of St Martin-in-the-fields, Middlesex, yeoman, and Audrey Anne, spinster - at same. License 21 Jan. 1624/5
+
+Recognizance:; Henry Rickson of St Martin in the Fields, yeoman (£10), Thomas White of St Martin in the Fields, tailor (£5) and Robert Jones of St Martin in the Fields, carpenter (£5) for the appearance of Henry Rickson at the next sessions and to keep the peace, especially regarding Richard Rogers; Taken before: Edward Forcett; Annotated: For the peace Defaulted Owes fees  WJ/SR(NS)2/064  18 James I [1620] 25 Mar.
+
+Obviously there are several Thomas White's and it is impossible to determine which of these was the one born in 1598, or if he was alive until the 1630s when our Thomas emigrated. However, he did not appear to die as an infant, and marriage records appear to show 2 of that name living in 1624 and one dying in 1625. Note that there is another possibility for our Thomas baptised in Westminster:
+
+Thomas Whyte s. of John, 14 July 1600, Saint Margaret, Westminster
+
+Thomas White of the parish of St Martins in the fields in the county of Middlesex Taylor left a will in 1673 naming his brothers John, Peter, and Mathew White. Whether he was the Thomas White, tailor, mentioned in the 1620 court record is uncertain, especially as he had several living brothers and sisters who would all have been old. However, those records and that of John White, tailor of Saint Clement Danes seems to show a family a number of whom were tailors.
+
+Records in Lower Norfolk County show in November 1651 a Mathewe White in a suit concerning a shipment of goods and that he was awarded payment for his transportation to his port and 3L/21s/6d (Walter, Lower Norfolk b, f. 201a). Also in November 1651 Henry Woodhouse was granted certificate for transportation of 8 people one of whom was Peter White (Walter, Lower Norfolk b, f. 207). In Feb. 1661/2 Henry Culpepper, Mary Cornix, and Peter White gave notice of their intended voyages to England (Brayton, Lower Norfolk D, f. 338a). Henry Culpepper could possibly be the same baptised 20 February 1632 at Saint Margaret, Westminster, although this is uncertain (http://gen.culpepper.com/ss/p1.htm). Could Mathew and Peter White who were for a time in Lower Norfolk but seemingly left be Thomas White's cousins mentioned in the will of Thomas White of Saint Martins?
+
+The court record cited above dated 1620 mentions a Robert Jones. A will of Robert Jones of St Andrew Holbourne dated 1619 mentions Thomas White, son of Robert White, connection to those in the court record unknown.
+
+The court record of Ralfe White of London in Lower Norfolk mentions a Robt. Causby. The other people in the record lived in Lower Norfolk, but possibly Robt. Causby was a London merchant. The will of Robert Cosbee of St Andrews Holburn was dated 1603. A Robert Cosbye was baptized at Saint James, Clerkenwell, 13 May 1621, son of Anthony, and this court record of Anthony dated 1613:
+
+Robert Greene of St. Bride's, —, and Francis Clement of St. Dunstan's-in-the-West, gentleman, for Anthony Cosbye of London, tailor, to appear.(http://www.british-history.ac.uk/report.aspx?compid=82308)
+
+So the best connection of Ralphe White to England aside from the fact he was of London appears to show a connection with west London area of Clerkenwell, St Andrew Holbourne, and St Brides.
+
+#################################################################
+
+Probably John Manning, baptized 13 Feb. 1616/7, was son of Robert Manning and Alice as the name of his mother is not given, but 2 children were baptized children of Robert and Alice Manning 1619 and 1623. St Clement burial records show Alice Manning wife of Robert was buried 1 March 1624/5. In 1626 Robert Manning and Elizabeth had one child. Then from 1627 to 1630 Robert and Martha Manning had 4 children, one of whom was Robert baptized 4 April 1630. So it is not clear, but probable that the same Robert Manning was the father of John and Robert.
+
+Manning Robert son of Robert citizen and poulterer to William King, 5 Dec 1643, Vintners' Company
+
+Apprentices needed to be 14 years of age to affirm, and we see that Robert Manning of St Clements would have been just that age if he was 4 months old at baptism. From this we infer that Robert Manning, father of John was a poulterer. No will was located for Robert Manning.
+
+The will of William Manning of the parish of St Clement Danes Butcher was made in 1623. Possibly he was related to Robert Manning. In it he made a bequest to Richard Hodges. The will of Richard Hodges of St Clement Danes Butcher was made in 1626 and made bequests to among others, 2 brothers named John Hodges. These wills show a connection of the Manning family and the Hodges family in Westminster, and further a connection to John Hodges, one of whom could have been the father of Roger Hodges.
+
+Two final records of  are the wills of William White of Saint Clement Danes Poulterer, dated 1658,  and his wife Cicely, dated 1673. It is interesting that he was a poulterer as was Robert Manning.
+
+Whight William son of William, St Clement Danes, Middlesex, tailor to Thomas Bromley, 6 Dec 1626, Vintners' Company
+
+A Thomas Bromley of St Benet's, Paul's Wharf, London, Vintner, made a will dated 12 Dec. 1633, almost exactly the 7 years needed for apprenticeship after he contracted with William White. So whether William White finished his apprenticeship and went on the become a vintner, or for some reason did not finish or instead became a poulter for some other reason is not clear.
+
+Also of interest is that his wife was the widow of a Mr. Bland and by him had a son Nicholas Bland. Possibly she had married a relative of the Bland family, one of whom John Bland owned shares of Virginia Company and had descendants living in Virginia. A Nicholas Bland son of Richard was born in London in 1590, and Adam Bland an ancestor of the Blands of Virginia had a son Richard born 1562 in London. I'm not sure if this is the correct genealogy, but a possible connection to a Virginia Company shareholder seems to make immigration of someone from this family more likely. One of the overseers of the will of Cicely was Richard Hodgkins, which could be an alternate spelling for Richard Hodges.
+
+My White ancestry descends from John White who lived in Lower Norfolk County and whose earliest record is in 1709 when he was granted a certificate for his own importation into Virginia. Due to the fact that he bought his farm on land that can be traced back to the 1600s and Thomas White, the most likely scenario is that he moved from Maryland and was a son of Thomas White's son John White. However, it is possible that he arrived in Lower Norfolk as an immigrant from England. The fact that Cicely and William White had a grandson named Jonathan White seems interesting in this respect since John White named sons William and Jonathan.
 
 
 
