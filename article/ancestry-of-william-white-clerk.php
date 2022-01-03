8b8baca84b9c7fc862a88ca2427e27f27e4aaf92
jim <jbwhite2@ncsu.edu>
2022-01-02 22:54:38 -0500

    templates

--- article/ancestry-of-william-white-clerk.php
+++ article/ancestry-of-william-white-clerk.php
@@ -29,10 +29,10 @@ include "../menu.php";
 <div class="row g-5 bg-light rounded">
   <div class="col-md-2"></div>
   <div class="col-md-8">
-    <h2>Home</h2>
-    William White Clerk died in York County, Virginia, in 1658. I used a record I found in the IGI to tentatively identify William White as being from Elworthy in Somerset. The will of Martha White, his widow, showed that he was the brother of Jeremiah White, minister. The fact that he was the brother of Jeremiah White has caused me to change his proposed ancestry to one that shows he was from Sempringham in Lincolnshire.
+    <h2>Ancestry Of William White, Clerk</h2>
+    <p>William White Clerk died in York County, Virginia, in 1658. I used a record I found in the IGI to tentatively identify William White as being from Elworthy in Somerset. The will of Martha White, his widow, showed that he was the brother of Jeremiah White, minister. The fact that he was the brother of Jeremiah White has caused me to change his proposed ancestry to one that shows he was from Sempringham in Lincolnshire.
 
-There is only one record in the university database for Jeremiah White:
+<p>There is only one record in the university database for Jeremiah White:
 <pre>White, Jeremy
 Adm. sizar at TRINITY, 07 Apr., 1646.
 B. 1629 .
@@ -48,13 +48,13 @@ Alley, Queen Street, Lower Rotherhithe [ London], after 1660 .
 A conspicuous member of the Calve's Head Club [ London].
 Author, religious.
 Died 11 Mar., 1707, aged 78.</pre>
-There are several possibilities for William White, one of which is the record in Somerset, but the one I now think is correct is:
+<p>There are several possibilities for William White, one of which is the record in Somerset, but the one I now think is correct is:
 <pre>White, William
 Matric. sizar from EMMANUEL, Easter 1642.
 Of Lincolnshire.
 Migrated to Queens', Aug., 1644.
 B.A. 1645/6 .</pre>
-I found records in the parish of Sempringham, Lincolnshire, that fit with these matriculation entries(Parish register of Sempringham, FHL film #0436046)
+<p>I found records in the parish of Sempringham, Lincolnshire, that fit with these matriculation entries(Parish register of Sempringham, FHL film #0436046)
 <pre>Baptisms:
 
 Phebe, dau of Thomas White Sept. 3, 1624
@@ -63,16 +63,16 @@ Dorcas, dau of Thomas White May 4, 1628
 Jeremye, son of Thomas White 21 Feb, 1629/30
 Phebe, dau of Thomas White Feb, 1631/2
 John, son of Mr. White Jan 14, 1633/4</pre>
-It is seen that these records are a perfect match of the known facts if William White matriculated when he was 17 as his brother was. The will of Thomas White was also located in PCC records.
+<p>It is seen that these records are a perfect match of the known facts if William White matriculated when he was 17 as his brother was. The will of Thomas White was also located in PCC records.
 <blockquote>Thomas White of Sempringham in the County of Lincoln Clerke being weake of body but of good memory blessed be god doe make this my last will and testament in manner and forme following First I commit my soule unto the hands of Almightie god expecting to be saved by the only merritte of Jesus Christ and my body I commit to the earth to be buried by the discretion of my executrix, And for my worldly goods I dispose them in this manner, First I give my tract [favens?] in Sutton in the County of Lincoln to my two eldet sonnes William and Jeremy to be equally possessed and enjoyed by them. Item I give all my bootes living [...?] to my said sonnes William and Jeremy to be equally divided between them Item I give to my well beloved Wife two hundred and fifty pounds, Item I give all the rest of my movable goods and chattel unto my five younger children to be equally divided amongst them by my [...?] hereafter named and the longer lived of them And if my wife be with child I wish that such child have an equal portion with my said five younger children and I desire Mr Jeremy Whittacres my very loving brother and Mr. Thomas Cary of Boston and Mr. John Brower of [...?] and Mr. John Holles of Sempringham to help my wife in the educacion of my said children and to improve my [...?] childrens [...?] and the said [Favens?] of Sutton for the advantage of my said children The said improvement to be given to my wife whence I make my sole executrix for theire maintenance. And I give to my loving friend Mr. Jove [...?], and I give unto my servants about theire due wages five shillings apiece, and I desire my loving friends above named to be overseers of this my will dated the Nynth day of August 1638.
 
 Probate written in Latin mentions Phebe White relicte</blockquote>
-Thomas White here called clerke, but no record was located in the univeristy database that seemed a match. On one of the pages in the parish register in 1626 was written Thomas White Curatt. A curate was an associate minister, so probably Thomas White was educated, but not with a university degree and not a full minister. Jeremy Whittacres mentioned as his loving brother was probably his church brother, and was possibly
+<p>Thomas White here called clerke, but no record was located in the univeristy database that seemed a match. On one of the pages in the parish register in 1626 was written Thomas White Curatt. A curate was an associate minister, so probably Thomas White was educated, but not with a university degree and not a full minister. Jeremy Whittacres mentioned as his loving brother was probably his church brother, and was possibly
 <a href="http://en.wikisource.org/wiki/Whitaker,_Jeremiah_%28DNB00%29">Jeremiah Whitaker</a>
 
-The land mentioned in Sutton suggests that Thomas White was from Sutton. There are 2 places in Lincolnshire named Sutton - Sutton on Sea and the area around Long Sutton, which contains several parishes with the name Sutton. There is a record of Thomas Wyte, son of Thomas Wyte born June 11, 1592 at Sutton Saint Edmund, which is one of the parishes near to Long Sutton. Possibly, this is the record for Thomas White Clerk of Sempringham. Long Sutton is in that part of Lincolnshire that is near to Norfolk and Cambridgeshire. Possibly Thomas White of Lower Norfolk was related to the Whites of Sutton, Lincolnshire, and lived nearby. Previous research has shown that Thomas Manning was from Norwich in Norfolk, and that it is possible that John Manning of Lower Norfolk who was Thomas White's neighbor was related and then might have been from the area around Norwich. If such is the case then it would make sense because Long Sutton is about 50 miles from Norwich.
+<p>The land mentioned in Sutton suggests that Thomas White was from Sutton. There are 2 places in Lincolnshire named Sutton - Sutton on Sea and the area around Long Sutton, which contains several parishes with the name Sutton. There is a record of Thomas Wyte, son of Thomas Wyte born June 11, 1592 at Sutton Saint Edmund, which is one of the parishes near to Long Sutton. Possibly, this is the record for Thomas White Clerk of Sempringham. Long Sutton is in that part of Lincolnshire that is near to Norfolk and Cambridgeshire. Possibly Thomas White of Lower Norfolk was related to the Whites of Sutton, Lincolnshire, and lived nearby. Previous research has shown that Thomas Manning was from Norwich in Norfolk, and that it is possible that John Manning of Lower Norfolk who was Thomas White's neighbor was related and then might have been from the area around Norwich. If such is the case then it would make sense because Long Sutton is about 50 miles from Norwich.
 
-Record of son John's apprenticeship in London: White John son of Thomas, Sempringham, Lincolnshire, clerk to Samuel Dabb 19 Mar 1649/50, Grocers' Company
+<p>Record of son John's apprenticeship in London: White John son of Thomas, Sempringham, Lincolnshire, clerk to Samuel Dabb 19 Mar 1649/50, Grocers' Company
 
 
 
