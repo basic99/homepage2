4b2be8b339ef0dad9484b82870397be270c0ef7c
jim <jbwhite2@ncsu.edu>
2022-01-03 21:32:30 -0500

    blog pages

---
+++ blog/reading-list-2012.php
@@ -0,0 +1,88 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+
+<p>READING LIST
+<p>THE BLACK BANNERS
+<p>JANUARY 10, 2012
+<p>Ali H. Soufan, The Black Banners, The Inside Story of 9/11 and the War Against al-Qaeda(New York: W.W. Norton, 2011). This  book, complete with redacted pages by the CIA, was  written by an Arabic speaking former FBI agent who interrogated many high level al-Qaeda captives. Before reading this book I didn’t really care too much about how al-Qaeda captives were interrogated. However, the writer makes a strong case from personal experience that he received more useful information without using enhanced interrogation techniques. Aside from that it gives a very good overview of the structure and growth of al-Qaeda in Sudan, Afghanistan, and Yemen, etc.
+
+<hr>
+
+<p>MAKERS OF MATHEMATICS
+<p>SEPTEMBER 24, 2011
+<p>Stuart Hollingdale, Makers of Mathematics (New York: Penguin Books, 1989) As title states a history of math. Equations are included, but are mostly just to illustrate points and generally I was not able to follow the math in his examples.
+<hr>
+
+<p>THE TEMPLARS
+<p>SEPTEMBER 24, 2011
+<p>Piers Paul Read, The Templars (New York: St. Martins, 1999) History of the Templars starting with background of the history of Jerusalem, and then the founding of the Templar order in 1119 in Jerusalem and their dissolution and trial at the hands of Phillip the Fair in 1307.
+
+<hr>
+<p>HTML 5 DESIGNING RICH INTERNET APPLICATIONS
+<p>JUNE 30, 2011
+<p>Matthew David, HTML 5 Designing Rich Internet Applications(Burlington, MA: Focal Press, 2010) Topic is obvious from the title. Good overview of using HTML 5 and its advanced features, some of which I probably won’t get around to using for a long time. Didn’t actually read all the material, but read enough to get me started with using HTML 5.
+
+<hr>
+<p>THE ELEMENTS OF TYPOGRAPHIC STYLE
+<p>JUNE 30, 2011
+<p>Robert Bringhurst, The Elements of Typographic Style (Vancouver: Hartley & Marks, 1992) Discussion of typography and page layout for books.
+
+<hr>
+<p>A HISTORY OF PI
+<p>JUNE 30, 2011
+<p>Petr Beckman, A History of Pi(New York: St. Martin’s Press, 1971) Mathematical history centered on the constant pi. Starting in ancient times, but emphasis on work of Archimedes, and on into modern times. Not much on computer techniques.
+
+<hr>
+<p>NUMBER THEORY AND ITS HISTORY
+<p>FEBRUARY 27, 2011
+<p>Oystein Ore, Number Theory and its History (1948; reprint, New York: Dover, 1988)
+
+<p>Good book for an introduction to number theory if you like math. Contains proofs, but avoids mathematical notation so its easier for amateurs. It took me about 2 1/2 months to finish the 360 page book.
+
+<hr>
+<p>THE MAN WHO LOVED ONLY NUMBERS
+<p>FEBRUARY 27, 2011
+<p>Paul Hoffman, The Man Who Loved Only Numbers, The Story of Paul Erdos and the Search for Mathematical Truth (New York: Hyperion, 1998)
+
+<p>This is a quick read about a famous and eccentric 20th century mathematician. He published many papers and there is a system called Erdos numbers where anyone who published a paper with him is given number one, anyone who publishes with that person a number 2, etc.
+
+
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
