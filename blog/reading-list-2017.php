94d6c2c4084480966a5631ea34fd54c4f879f174
jim <jbwhite2@ncsu.edu>
2022-01-04 13:11:42 -0500

    reading list

---
+++ blog/reading-list-2017.php
@@ -0,0 +1,142 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+
+<p>THE VIKING WARRIOR
+<p>DECEMBER 23, 2017
+<p>Ben Hubbard, The Viking Warrior, The Raiders, Pillagers, and explorers who terrorized Medieval Europe (London: Amber Books, 2017). A picture book of Viking history.
+
+<hr>
+<p>HEART OF DARKNESS
+<p>DECEMBER 23, 2017
+<p>Joseph Conrad, Heart of Darkness (New York: Barnes & Noble, 2003). Fiction but based on the experiences of the author into Belgian Congo. Paints a grim picture of imperialism in Africa.
+
+<hr>
+<p>THE TRAVELS OF MARCO POLO
+<p>DECEMBER 23, 2017
+<p>Marco  Polo, The Travels of Marco Polo (New York: Fall River Press, 2012). A coffee table style edition of the Yule Cordier translation of the works of Marco Polo.
+
+<hr>
+<p>THE ORIGINS OF TOTALITARIANISM
+<p>DECEMBER 9, 2017
+<p>Hanna Arendt, The Origins of Totalitarianism (New York: Harcourt, 1951). I was assigned this book as a freshman in college and read the first 50 pages. Saw it in the bookstore and gave it another try. Rather long, deep and slow-reading ( I often read a passage twice). Very insightful and thoughtful analysis of the form of government of Hitler and Stalin.
+
+<hr>
+<p>STALIN, NEW BIOGRAPHY OF A DICTATOR
+<p>DECEMBER 9, 2017
+<p>Oleg V. Khlevniuk, translated by Nora Seligman Favorov, Stalin, New Biography of a Dictator (New Haven: Yale University Press, 2015). An interesting medium length biography. As it is a translation it at times was like reading the news, but was generally very readable and informative.
+
+<hr>
+<p>THE ODYSSEY
+<p>APRIL 17, 2017
+<p>Robert Fagles, The Odyssey by Homer (New York: Penguin Books, 1996).  This book is shown on a web site called The Greatest Books as the fourth on the list and the third on the list is supposed to be based on it.
+
+<hr>
+<p>MARCO POLO, THE JOURNEY THAT CHANGED THE WORLD
+<p>APRIL 17, 2017
+<p>John Man, Marco Polo, The Journey that Changed the World (New York: Harper Collins, 2009).
+
+<hr>
+<p>THE SIXTH EXTINCTION: AN UNNATURAL HISTORY
+<p>APRIL 16, 2017
+<p>Elizabeth Kolbert, The Sixth Extinction: An Unnatural History (New York: Henry Holt, 2014). A discussion of past extinction events in earth’s geological past and the idea that humans are currently being the cause of a new extinction event.
+
+<hr>
+<p>UNDERSTANDING COMPRESSION
+<p>APRIL 16, 2017
+<p>Colt McAnlis & Aleks Haecky, Understanding Compression (Sebastopol, Calif.: O’Reilly, 2016). A discussion of some algorithms used by computers to compress data.
+
+<hr>
+<p>1776
+<p>APRIL 16, 2017
+<p>David McCullough, 1776 (New York: Simon & Schuster, 2005). An historical account of the first year of the American Revolution.
+
+<hr>
+<p>THE TROJAN WAR, A NEW HISTORY
+<p>APRIL 16, 2017
+<p>Barry Strauss, The Trojan War, A New History (New York: Simon & Schuster, 2006). An historical account of the Trojan War of the Iliad.
+
+<hr>
+<p>GENGHIS KHAN AND THE MAKING OF THE MODERN WORLD
+<p>APRIL 16, 2017
+<p>Jack Weatherford, Genghis Khan and the Making of the Modern World (New York: Broadway Books, 2004).  I visited a Genghis Khan exhibit at the Discovery Place museum in Charlotte, and bought this book in the bookstore on the way out.
+
+<hr>
+<p>THE ANCESTOR’S TALE, A PILGRIMAGE TO THE DAWN OF EVOLUTION
+<p>JULY 25, 2016
+<p>Richard Dawkins, The Ancestor’s Tale, A Pilgrimage to the Dawn of Evolution (New York: Mariner, 2004). A rather long book. It starts with humans and goes back in time to ancestors we shared with other things that are living today. A chapter for each common ancestor as you go back in time to common ancestor of fish, plants, fungi, bacteria, etc. At each chapter he uses it to illustrate some points about evolution.
+
+<hr>
+<p>MAGICAL MUSHROOMS, MISCHEVIOUS MOLDS
+<p>JULY 25, 2016
+<p>George Hudler, Magical Mushrooms, Mischevious Molds (Princeton, 1998). Book I read as an introduction to fungi because I work in a lab that does research on fungi.
+
+<hr>
+<p>THE SELFISH GENE
+<p>APRIL 2, 2016
+<p>Richard Dawkins, The Selfish Gene (Oxford, 1989). Probably Dawkins’ most popular and important work. Attempts to explain behaviour by what is most likely to transmit a gene.
+
+<hr>
+<p>THE AENEID
+<p>APRIL 2, 2016
+<p>Robert Fagles (translator) The Aeneid by Virgil (New York: Penguin, 2006). Classic work,  one of the most famous books ever written. Decided to read it after seeing it mentioned several times in the book by Mary Beard about Rome.
+
+<hr>
+<p>YOUR INNER FISH
+<p>APRIL 2, 2016
+<p>Neil Shubin, Your Inner Fish A Journey into the 3.5-billion-year History of the Human Body (New York: Vintage, 2008). The writer describes his discovery of the fossil record of the missing link between fish and land animals. He also describes the anatomical similarity of fishes and other animals in development of an embryo.
+
+<hr>
+<p>NEANDERTHAL MAN IN SEARCH OF LOST GENOMES
+<p>APRIL 2, 2016
+<p>Svante Paabo, Neanderthal Man In search of Lost Genomes (New York: Basic Books, 2014). Author writes about his lab’s research to extract dna from Neanderthal remains.
+
+<hr>
+<p>THE RED QUEEN: SEX AND THE EVOLUTION OF HUMAN NATURE
+<p>APRIL 2, 2016
+<p>Matt Ridley, The Red Queen: Sex and the Evolution of Human Nature (New York: Harper). The book starts at a genetic level giving theories for the evolution of sex and genetic recombination. Then it looks at sexual activity of animals and how it can be explained by its effect on transmission of genes. Lastly, he develops some theories explaining sexual preferences and habits of humans.
+
+<hr>
+<p>INFINITESIMAL: HOW A DANGEROUS MATHEMATICAL THEORY SHAPED THE MODERN WORLD
+<p>APRIL 2, 2016
+<p>Amir Alexander, Infinitesimal: How a Dangerous Mathematical Theory Shaped the Modern World (New York: MacMillan). This book is a history of the use of infinitesimal in math. It starts in Italy with conflict of Galileo and the Jesuit Catholics over the use of infinitesimals in math. The second part of the book covers the same debate in England between Thomas Hobbes and the English promoters of the use of infinitesimals.
+
+
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
