8e6c20263a24fe718a3f7158427667575c9350dc
jim <jbwhite2@ncsu.edu>
2022-01-04 12:56:55 -0500

    reading list

---
+++ blog/reading-list-2013.php
@@ -0,0 +1,141 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+
+<p>DARWIN’S GHOST: THE ORIGIN OF SPECIES UPDATED
+<p>MARCH 31, 2014
+<p>Steve Jones, Darwin’s Ghost: The Origin of Species Updated (New York: Random House, 2000). Book written by an academic geneticist in the form of Darwin’s The Origin of Species, but with the knowledge of biology of a modern scientist.
+
+<hr>
+<p>DID JESUS EXIST?: THE HISTORICAL ARGUMENT FOR JESUS OF NAZARETH
+<p>MARCH 31, 2014
+<p>Bart D. Ehrman, Did Jesus Exist?: The Historical Argument for Jesus of Nazareth (New York, Harper Collins, 2012) A book by an academic historian and agnostic/atheist presenting the case for existence of historical Jesus and some historical facts that can be surmised. Written in reply to books that make a case that Jesus was not a real historical person.
+
+<hr>
+<p>ZEALOT: THE LIFE AND TIMES OF JESUS OF NAZARETH
+<p>MARCH 31, 2014
+<p>Reza Aslan, Zealot: The Life and Times of Jesus of Nazareth (New York, Random House, 2013) Interesting and mostly well written book about historical Jesus. Parts of it were written more for dramatic effect than historical truth. Basic thesis that Jesus’ main aim was the overthrow of Roman rule and establishment of Jewish religious rule is not consistent with prevailing academic view that he was an apocalyptic prophet(per Ehrman).
+
+<hr>
+<p>RESTFUL WEB SERVICES
+<p>SEPTEMBER 8, 2013
+<p>Leonard Richardson and Sam Ruby, Restful Web Services (Sebastopol: O’Reilly, 2007) Technical book, have finished first half and skimmed rest/reference material. I recently took a week long course in using Python for web programming and this was the only book recommended aside from books about Python so I bought it. It is a method to provide web services that tries to work with  and use technologies implemented by the  http standards. The competing ways to do this are SOAP and XML/RPC which embeds function like calls and metadata in the request body.
+
+<hr>
+<p>WATCHING BASEBALL SMARTER
+<p>SEPTEMBER 8, 2013
+<p>Zack Hample, Watching Baseball Smarter (New York: Vintage Books, 2007) Good review of rules, strategy, pitching, etc. for baseball fans.
+
+<hr>
+<p>THE HOUSE OF MEDICI: ITS RISE AND FALL
+<p>SEPTEMBER 8, 2013
+<p>Christoper Hibbert, The House of Medici: Its Rise and Fall (New York: Morrow Quill, 1980) Renaissance, Florence, and Medici are all interrelated in this history of the Medici family. Story starts in 15th century and the rise of the Florentine  banking family, their becoming nobility and through to the last of the line in the 18th century.
+
+<hr>
+<p>FULL HOUSE
+<p>SEPTEMBER 8, 2013
+<p>Stephen Jay Gould, Full House (New York: Random House, 1996) Author weaves together his interest in baseball and the theory of evolution. Somehow manages to relate Ted Williams batting average to the importance of bacteria in understanding evolution.
+
+<hr>
+<p>THE STORY OF EARTH
+<p>SEPTEMBER 8, 2013
+<p>Robert M. Hazen, The Story of Earth (New York: Penguin, 2011) History of earth from formation ~4.6 billion years ago told by a geologist/paleontologist. Story is up to date with latest theories as is written by a professional in this field. Interesting story, recommended.
+
+<hr>
+<p>INTO THE HINTERLANDS
+<p>SEPTEMBER 8, 2013
+<p>David Drake and John Lambshead, Into the Hinterlands (Riverdale, NY: Baen, 2011). Science fiction story, outer space in the future. Flying through space-time on bicycles, fighting silicon based life forms, etc.
+
+<hr>
+<p>THE GOD DELUSION
+<p>MARCH 4, 2013
+<p>Richard Dawkins, The God Delusion (New York: Houghton Mifflin, 2006) A prominent biologist writes about his belief in the non-existence of God, especially the God of Jews, Christians and Muslims. This book was listed as one to be discussed by the Unitarian church theology book club, and I read it and went to those discussions.
+
+<p>INTRODUCTION TO GRAPH THEORY
+<p>MARCH 4, 2013
+<p>Richard J. Trudeau, Introduction to Graph Theory(New York: Dover Publications, 1993) This is one of those nice math books that is made easier to follow without dumbing down. Not sure what practical uses the material has, but probably has some applications in computers and other areas. Basically, it discusses points and lines between them(edges and vertices) and topics such as whether you can draw the graph in the plane. A rather famous problem of this branch of math is the map coloring problem. The fact that a map can be covered with 5 colors is proved in the book, and the 4 color has been recently proved with the help of computers.
+
+<hr>
+<p>CHURCHILL: A STUDY IN GREATNESS
+<p>DECEMBER 5, 2012
+<p>Geoffrey Best, Churchill: A Study in Greatness(New York: Oxford University, 2003) Medium length (336 pages) biography of Churchill. Great man, good book.
+
+<hr>
+<p>THE ORIGINS OF THEORETICAL POPULATION GENETICS
+<p>DECEMBER 5, 2012
+<p>William B. Provine, The Origins of Theoretical Population Genetics(London: University of Chicago, 2001) As title suggests its a history of theoretical population genetics. In 1900 when the laws of Mendelian inheritance were re-discovered biologists were unable to reconcile them with the ideas of Darwinian natural selection. Most of the book (its not a long book – less than 200 pages) is concerned with the debates from 1900 to around 1930 during which time these 2 approaches were reconciled and modern population genetics emerged.
+
+<hr>
+<p>A HISTORY OF THE ARAB PEOPLES
+<p>AUGUST 9, 2012
+<p>Albert Hourani, A History of the Arab Peoples(New York: Warner Books, 1991) History starting with Arab peoples before the birth of Muhammed, his birth and history, Baghdad, Muslim society, the Ottoman empire and into the post WWII era. A little slow reading at times, but shows an important side of history to people raised in western culture.
+
+<hr>
+<p>JULIUS CAESAR
+<p>AUGUST 9, 2012
+<p>J.F.C. Fuller, Julius Caesar Man, Soldier, and Tyrant(New York: Da Capo, 1965) Story of Julius Caesar starting with history of Rome in the 100 years before his rise, and continuing through his Gallic Wars and the Civil War to his death. I happened to watch the movie Cleopatra while reading the book and it was interesting to compare them. The movie is actually pretty historic.
+
+<hr>
+<p>THE ENGLISH CIVIL WAR
+<p>APRIL 28, 2012
+<p>Diane Purkiss, The English Civil War(New York: Perseus Books, 2006). Interesting history of the historical background of the English Civil War and the war itself in the 1640s. Sometimes there was a lot of quoting of old letters and family details to show what it was really like for people, but also a lot of interesting historical detail I had not heard.  Not a great deal of military history, just several pages on each of  the major battles.
+
+<hr>
+<p>PROFESSIONAL WORDPRESS PLUGIN DEVELOPMENT
+<p>APRIL 21, 2012
+<p>Brad Williams, Ozh Richard, and Justin Tadlock, Professional WordPress Plugin Development (Indianapolis: Wiley, 2011). I read the first half of the book and parts of the second half that I needed to write my first WordPress plugin which I used to create map pages which can be seen under the computer section of this site.
+
+<hr>
+<p>CHAOS, MAKING A NEW SCIENCE
+<p>MARCH 3, 2012
+<p>James Gleick, Chaos Making a New Science (New York: Penguin Books, 1987). Interesting story about advances in understanding non-linear dynamics and applications to many different areas of science.
+
+<hr>
+<p>ON BASILISK STATION
+<p>MARCH 3, 2012
+<p>David Weber, On Basilisk Station (New York, 1993). Outer space science fiction.
+
+<hr>
+<p>THE QUANTUM STORY
+<p>MARCH 3, 2012
+<p>Jim Baggott, The Quantum Story (Oxford, 2011) A history of quantum mechanics from Planck to the present day.
+
+
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
---
+++ blog/reading-list-2017.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+
+<p>THE TROJAN WAR, A NEW HISTORY
+<p>APRIL 16, 2017
+<p>Barry Strauss, The Trojan War, A New History (New York: Simon & Schuster, 2006). An historical account of the Trojan War of the Iliad.
+
+<hr>
+<p>GENGHIS KHAN AND THE MAKING OF THE MODERN WORLD
+<p>APRIL 16, 2017
+<p>Jack Weatherford, Genghis Khan and the Making of the Modern World (New York: Broadway Books, 2004).  I visited a Genghis Khan exhibit at the Discovery Place museum in Charlotte, and bought this book in the bookstore on the way out.
+
+<hr>
+<p>THE ANCESTOR’S TALE, A PILGRIMAGE TO THE DAWN OF EVOLUTION
+<p>JULY 25, 2016
+<p>Richard Dawkins, The Ancestor’s Tale, A Pilgrimage to the Dawn of Evolution (New York: Mariner, 2004). A rather long book. It starts with humans and goes back in time to ancestors we shared with other things that are living today. A chapter for each common ancestor as you go back in time to common ancestor of fish, plants, fungi, bacteria, etc. At each chapter he uses it to illustrate some points about evolution.
+
+<hr>
+<p>MAGICAL MUSHROOMS, MISCHEVIOUS MOLDS
+<p>JULY 25, 2016
+<p>George Hudler, Magical Mushrooms, Mischevious Molds (Princeton, 1998). Book I read as an introduction to fungi because I work in a lab that does research on fungi.
+
+<hr>
+<p>THE SELFISH GENE
+<p>APRIL 2, 2016
+<p>Richard Dawkins, The Selfish Gene (Oxford, 1989). Probably Dawkins’ most popular and important work. Attempts to explain behaviour by what is most likely to transmit a gene.
+
+<hr>
+<p>THE AENEID
+<p>APRIL 2, 2016
+<p>Robert Fagles (translator) The Aeneid by Virgil (New York: Penguin, 2006). Classic work,  one of the most famous books ever written. Decided to read it after seeing it mentioned several times in the book by Mary Beard about Rome.
+
+<hr>
+<p>YOUR INNER FISH
+<p>APRIL 2, 2016
+<p>Neil Shubin, Your Inner Fish A Journey into the 3.5-billion-year History of the Human Body (New York: Vintage, 2008). The writer describes his discovery of the fossil record of the missing link between fish and land animals. He also describes the anatomical similarity of fishes and other animals in development of an embryo.
+
+<hr>
+<p>NEANDERTHAL MAN IN SEARCH OF LOST GENOMES
+<p>APRIL 2, 2016
+<p>Svante Paabo, Neanderthal Man In search of Lost Genomes (New York: Basic Books, 2014). Author writes about his lab’s research to extract dna from Neanderthal remains.
+
+<hr>
+<p>THE RED QUEEN: SEX AND THE EVOLUTION OF HUMAN NATURE
+<p>APRIL 2, 2016
+<p>Matt Ridley, The Red Queen: Sex and the Evolution of Human Nature (New York: Harper). The book starts at a genetic level giving theories for the evolution of sex and genetic recombination. Then it looks at sexual activity of animals and how it can be explained by its effect on transmission of genes. Lastly, he develops some theories explaining sexual preferences and habits of humans.
+
+<hr>
+<p>INFINITESIMAL: HOW A DANGEROUS MATHEMATICAL THEORY SHAPED THE MODERN WORLD
+<p>APRIL 2, 2016
+<p>Amir Alexander, Infinitesimal: How a Dangerous Mathematical Theory Shaped the Modern World (New York: MacMillan). This book is a history of the use of infinitesimal in math. It starts in Italy with conflict of Galileo and the Jesuit Catholics over the use of infinitesimals in math. The second part of the book covers the same debate in England between Thomas Hobbes and the English promoters of the use of infinitesimals.
+
+<hr>
+<p>SPQR A HISTORY OF ANCIENT ROME
+<p>APRIL 2, 2016
+<p>Mary Beard, SPQR A History of Ancient Rome (New York: Liveright Publishing) Kindle.  A history of Rome from its founding in pre-historic times up until the start of the 3rd century.
+
+<hr>
+<p>THE RISE OF THE TUDORS
+<p>APRIL 25, 2015
+<p>Chris Skidmore, The Rise of the Tudors, The Family that Changed English History (New York: St. Martins Press). Kindle edition. The story of the events leading to the Battle of Bosworth Field and the start of the Tudor kings and queens of England. Final chapter and postscript deal with recent archaeological searches for the site of the battle field and the discovery of the skeleton of Richard III.
+
+<hr>
+<p>THE BRUS FAMILY
+<p>APRIL 25, 2015
+<p>Ruth M. Blakely, The Brus Family in England and Scotland 1100-1295 (Woodbridge: The Boydell Press, 2005). My genealogy research has found that I might be related to this family(Humphrey White md. Elizabeth Bruce, 1586). The family in England is descended from Robert Brus who came from France around 1100 and was granted lands in the north of England. HIs sons Adam and Robert established the 2 branches of this family, the Lords of Annandale in Scotland from which the Scottish king is descended and the Lords of Skelton in Yorkshire. The book is well researched and seems to give an accurate history of the early years of this family. The Lords of Annandale were owners of the Manor of Writtle in the late 13th century, but it was taken away when Robert became king in 1306.
+
+<hr>
+<p>THE MANOR OF WRITTLE
+<p>APRIL 25, 2015
+<p>K.C. Newton, The Manor of Writtle (London: Phillimore, 1970). History of the Manor Writtle in Essex, mostly concerned with records of the Manor in the medieval times. Its related to my genealogy interests as it contains Roxwell the burial place of Humphrey White, and also the sub-manor of Benedict Otes which was owned by Thomas Varvell, a merchant with connections to Virginia.
+
+<hr>
+<p>BOCKING DEANERY
+<p>APRIL 25, 2015
+<p>Ann Hoffmann, Bocking Deanery, The Story of an Essex Peculiar(London: Phillimore, 1976). A history book I read related to my genealogy research interests in Essex.
+
+<hr>
+<p>THE DEMON-HAUNTED WORLD
+<p>APRIL 25, 2015
+<p>Carl Sagan, The Demon-Haunted World, Science as a Candle in the Dark (New York, 1996). Book read with Unitarian book club.
+
+<hr>
+<p>THE ENGLISH VILLAGE
+<p>APRIL 25, 2015
+<p>Richard Muir, The English Village (New York: Thames and Hudson, 1980). Picture book with text. History and description of English Village from earliest times.
+
+<hr>
+<p>MERCHANTS AND REVOLUTION
+<p>MAY 23, 2014
+<p>Robert Brenner, Merchants and Revolution – Commercial Change, Political Conflict, and London’s Overseas Traders, 1550-1653 (Princeton, 1993). The book describes the English merchants and their effects on politics starting with the Merchant Adventurers and their regulated cloth trade through to the new merchants trading largely in America and their influence on the civil war. It is a long and academic book (637 pages excluding the postscript which itself was another 80 pages which I skipped), but a good overview of its field and political forces at work in the civil war.
+
+<hr>
+<p>SCIENCE AND THE SEARCH FOR GOD
+<p>MARCH 31, 2014
+<p>Gary A. Kowalski, Science and the Search for God (New York: Lantern Books, 2003). Written by Unitarian minister attempting to present a theology that is not in conflict with modern scientific knowledge.
+
+<hr>
+<p>GARDEN OF MICROBIAL DELIGHTS
+<p>MARCH 31, 2014
+<p>Dorion Sagan and Lynn Margulis, Garden of Microbial Delights: A Practical Guide to the Subvisible World (Dubuque: Kendall Hunt, 1993). Picture book introduction to bacteria, protoctists, and fungi.
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
