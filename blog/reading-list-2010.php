0c4c47e62ee447895d43516047edaba251b7587f
jim <jbwhite2@ncsu.edu>
2022-01-03 21:27:33 -0500

    blog pages

---
+++ blog/reading-list-2010.php
@@ -0,0 +1,197 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+
+
+<p>UNCOMMON GROUNDS
+<p>DECEMBER 12, 2010
+<p>Mark Pendergrast, Uncommon Grounds, The History of Coffee and How it Transformed our World, Revised Edition (New York: Basic Books, 2010)
+
+<p>The book is concerned with the history of coffee and not much about drinking and fixing coffee. Mostly it discusses the production, sales and marketing after the American Civil War. Consequently there is a lot of detail about Brazilian economics and politics and the sales and marketing in the US. It is concerned mostly with the growth of large coffee companies in the first of the twentieth century, but does document the rise of specialty coffee in the last part of the book.
+
+<p>There is a short section on drinking coffee that recommends using freshly roasted beans so have found a local roaster and am trying some to fix coffee how the experts drink it.
+
+<hr>
+
+<p>THE LIFE OF DAVID HUME
+<p>DECEMBER 5, 2010
+<p>E. C. Mossner, The Life of David Hume, second edition (Oxford: Oxford, 1980)
+
+<p>A lengthy but good book about David Hume.
+
+<hr>
+<p>DISCOVERIES AND OPINIONS OF GALILEO
+<p>DECEMBER 5, 2010
+<p>Discoveries and Opinions of Galileo, trans. Stillman Drake (New York: Anchor Books, 1957)
+
+<p>Essays and letters of Galileo with introductory essays by the translator. Account of the invention of the telescope and his struggles with the church.
+
+<hr>
+<p>CANDIDE OR, OPTIMISM
+<p>DECEMBER 5, 2010
+<p>Voltaire, Candide or Optimism, trans. Peter Constantine (New York: Modern Library, 2005)
+
+<p>A famous book. I enjoyed reading it.
+<hr>
+
+<p>A DISCOURSE ON INEQUALITY
+<p>DECEMBER 5, 2010
+<p>Jean-Jacques Rousseau, A Discourse on Inequality, trans. Maurice Cranston (New York: Penguin Classics, 1984)
+
+<p>I am not a political scientist, but I think the influence of Rousseau might have been greater in his own time than it is today. Perhaps the random musings of a 18th century radical.
+
+<hr>
+<p>THE REALM OF THE NEBULAE
+<p>DECEMBER 5, 2010
+<p>Edwin Hubble, The Realm of the Nebulae (1936; reprint, New Haven: Yale, 1982)
+
+<p>This is a good book. It describes the observational astronomy that led to some understanding of the size of the universe.
+
+<hr>
+<p>UNKNOWN QUANTITY
+<p>NOVEMBER 30, 2010
+<p>John Derbyshire, Unknown Quantity, A Real and Imaginary History of Algebra (New York: Plume, 2006)
+
+<p>To quote the start of Chapter 11 – “Mathematics is, let’s face it, a dry subject, with little in the way of glamour or romance. The story of Evariste Galois has therefore been made much of by historians of Math.”
+
+<p>He was killed at the age of 21 in a duel in France in 1830, and the night before he died he wrote a math paper that provided the foundations for Galois Theory.
+
+<p>So that’s the most interesting part of the book and its difficult to see where he’s going if you are not a mathematician. However, it is an important story and he does a good job with the subject it seems to me – while giving account of the theoreticians who were discovering the foundations for advanced math.
+
+<hr>
+<p>WELCOME TO THE CHURCH YEAR
+<p>NOVEMBER 30, 2010
+<p>Vicki K. Black, Welcome to the Church Year, An Introduction to the Seasons of the Episcopal Church (Harrisburg, PA: Morehouse, 2004)
+
+<p>While reading a book about medieval history in which many dates were given in church year, I decided I needed to refresh my knowledge in this area.
+
+<hr>
+<p>ELEANOR OF AQUITAINE AND THE FOUR KINGS
+<p>NOVEMBER 30, 2010
+<p>Amy Kelly, Eleanor of Aquitaine and the Four Kings (Cambridge: Harvard, 1950)
+
+<p>This book is a history, but reads like an historical novel. Set mostly in 12th century France it concerns Eleanor who was married to 2 kings, and was mother to 2 kings. I had years ago read a history book about the reign of Henry II of England, but this was I thought a better book and I learned more from it.
+
+<p>I found this book at the used book store near campus.
+
+<hr>
+<p>THE POWER OF BABEL
+<p>NOVEMBER 30, 2010
+<p>John McWhorter, The Power of Babel (New York: Harper-Collins, 2001)
+
+<p>Interesting book by a well known linguist.
+
+<hr>
+<p>THE VITAMIN D REVOLUTION
+<p>NOVEMBER 30, 2010
+<p>Soram Khalsa, M.D., The Vitamin D Revolution, How the Power of This Amazing Vitamin Can Change Your Life (Carlsbad, CA: Hay House, 2009)
+
+<p>Lots of evidence for the beneficial effects of Vitamin D. Be tested or take 2000 IU/day.
+
+<hr>
+<p>ADAM SMITH
+<p>NOVEMBER 26, 2010
+<p>Jerry Z. Muller, Adam Smith In His Time and Ours (Princeton: Princeton, 1993)
+
+<p>Adam Smith is more famous as the founder of economics. This is an interesting long essay on the philosophy of Adam Smith.
+<hr>
+<p>MARATHONING FOR MORTALS
+<p>NOVEMBER 26, 2010
+<p>John “The Penguin” Bingham, Coach Jenny Hadfield, Marathoning for Mortals, A Regular Person’s Guide to the Joy of Running or Walking a Half-marathon or Marathon (New York: Rodale, 2003)
+
+<p>Content is obvious from title. Philosophically the emphasis of the authors is doing the training, and showing up for the race. The main emphasis is showing up at the start line, then the race will just happen.
+
+<hr>
+<p>THE CODE BOOK
+<p>NOVEMBER 26, 2010
+<p>Simon Singh, The Code Book, The Evolution of Secrecy from Mary Queen of Scots to Quantum Cryptography (New York: Doubleday, 1999)
+
+<p>Interesting book which shows the importance of cryptography from early times through WW II and the breaking of Nazi code machines to present day.
+<hr>
+
+<p>ESSENTIAL PHP SECURITY
+<p>NOVEMBER 26, 2010
+<p>Chris Shiflett, Essential PHP Security (Sebastopol, CA: O’Reilly, 2006)
+
+<p>A short book I bought several years ago, but found useful for studying for my PHP certification exam.
+
+<hr>
+<p>SELINUX BY EXAMPLE
+<p>NOVEMBER 26, 2010
+<p>Frank Mayer, Karl MacMillan, and David Caplan, SELinux by Example, Using Security Enhanced Linux (Upper Saddle River, NJ: Prentice Hall, 2007)
+
+<p>Technical book about security software I installed on our servers at work.
+
+<hr>
+<p>DESIGNING WITH WEB STANDARDS
+<p>NOVEMBER 18, 2010
+<p>Jeffrey Zeldman with Ethan Marcotte, Designing with Web Standards Third Edition (Berkeley: New Riders, 2010)
+
+<p>An important book by an important writer in this field (or so I have been told). Many tips and ideas appear in my newly redesigned homepage.
+
+<hr>
+<p>CRYPTOGRAPHY THEORY AND PRACTICE
+<p>NOVEMBER 18, 2010
+<p>Douglas R. Stinson, “Cryptography Theory and Practice Second Edition” (Boca Raton: Chapman & Hall/CRC, 2002)
+
+<p>Technical reading about this topic. Haven’t actually read the last page of the last chapter, but have read enough to consider to have finished it. If you aren’t convinced that public key infrastructure really works or want to know how secure encryption is, then this is a good book.
+
+<hr>
+<p>THE FUTURE OF LIFE
+<p>NOVEMBER 18, 2010
+<p>Edward O. Wilson, “The Future of Life” (New York: Vintage Books, 2002)
+
+<p>Quick read by a famous writer.
+
+<hr>
+<p>EVERY LIVING THING
+<p>NOVEMBER 18, 2010
+<p>Rob Dunn, “Every Living Thing, Man’s Obsessive Quest to Catalog Life, from Nanobacteria to New Monkeys,” (New York: Harper Collins, 2009)
+
+<p>Written by a professor in our department. I learned more from this book about zoology than  a college graduate should.
+
+<hr>
+<p>CROWDED WITH GENIUS
+<p>NOVEMBER 18, 2010
+<p>James Buchanan, “Crowded with Genius, The Scottish Enlightenment: Edinburgh’s Moment of the Mind,” (New York: Harper Collins, 2003)
+
+<p>Pretty good book. A lot of the names of people in it were unfamiliar to me.
+
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
