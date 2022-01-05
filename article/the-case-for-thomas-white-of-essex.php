815d8149d2ef5db4aab473f9c3a1050dedd11af3
jim <jbwhite2@ncsu.edu>
2022-01-04 22:18:57 -0500

    article

---
+++ article/the-case-for-thomas-white-of-essex.php
@@ -0,0 +1,85 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+    <h2>The Case for Thomas White of Essex</h2>
+
+    <p>My search for the British origins of Thomas White led me to the research of Ralph White of London who appeared in Lower Norfolk records in the 1650s. I have considered possible Ralph Whites of London in my article <a title="White, Manning, Hodges London Connection" href="/article/white-manning-hodges-london-connection.php">White, Manning, Hodges London Connection </a>and now think the most likely identification is that he was Ralph White, grocer of Stepney. Research has shown that he was born in 1598 at South Weald in Essex. He had a brother named Thomas who was born in 1602, but research has ruled him out as Thomas of Virginia. However, there is another parish named Willingale Doe, about 7 miles to the north of South Weald measured by closest approach. <a  href="/img/essex_map2.png">Map of Essex parishes</a>
+<figure class="figure">
+<img class="img-fluid" src="http://jimserver.net/graphics/thomas_1598.png" alt=""  /></figure>
+
+<p>As can be seen in the parish register of Willingale Doe, Thomas, son of Humffray Whighte was baptized 18 Feb. 1598/9. This date gives the correct age for Thomas White's deposition 15th July 1657 when he gave his age as 58.
+
+Ralph White left 3 records that I know of in Virginia. First in 1653 it was orderd he pay a debt in Lower Norfolk court. He appears to be in Virginia at this time. Second was his record in Lower Norfolk court of 1656 where he was referred to as Ralph White of London. It mentions that part of his debt was paid by Robert Causby. Finally, in 1664 his name was one of 11 headrights on a grant to John Stith in Charles City Count, and tending to confirm that he was in Virginia in 1653.
+
+The first thing about the will or <a  href="/article/london-wills.php/#ralphwhite">Ralph White</a>, grocer of Stepney, that I noticed that led me to suspect he was the one who appeared in Virginia records was the name of Thomas Lambert as a witness. A Thomas Lambert was a justice in Lower Norfolk County and in 1662 purchased part ownership of a ship from Thomas Alexander so some connection with Stepney and mariners seems possible. In the will of Elizabeth Lloyd of Lower Norfolk County, dated 1656/7, she gave Lieutenant Colonel Thomas Lambert 2000 pounds tobacco, and to Rachell Lambert, daughter of Thomas Lambert, 5 pounds to pay her voyage to Virginia. The parish records of Saint Dunstan, Stepney, show a Rachel Lambert born to Thomas Lambert, mariner, and Sara in 1633. There is also a christening of Thomas to Thomas Lambert and Sarah at Saint Dunstan in 1638, and an apprenticeship of Thomas Lambert , son of Thomas of Stepney, mariner, in 1661. This is somewhat older than the usual apprenticeship, but it is probably the same person. Thomas Lambert of Lower Norfolk was present in Lower Norfolk court on 15 Oct. 1661, four days after Ralph White's will was written, so it would not have been him, but it seems possible that it was his son who was still living in England. Another Thomas Lambert, son of John, was baptised as Saint Dunstan in 1642, so perhaps the senior designation was to differentiate these 2 persons.
+
+The court record on Lower Norfolk County suggests that Robert Causby might have been a London merchant. The will of '<a  href="/article/london-wills.php/#crosbie">Robert Crosbie</a> of the parish of Allhallows Barking London cittizen and Cooke of London', dated 10 August 1657, mentions that he owned property in Stepney. Crosbie is different from the spelling of the court record as Causby, but I understand that these variants could be the same name. If he were the same as mentioned in the court record it would support the case of Ralph White of Stepney.
+
+Finally, Ralph White appointed as his executor Alexander Harwood citizen and mercer of London. The will <a  href="/article/london-wills.php/#harwood">Arthur Harwood</a>, brother of Alexander, shows that he lived for a time in Virginia.
+
+Ralph White and his brothers William and Samuel were all citizens of London, and should have had apprentice records. I was able to locate the record for William White in the Haberdashers Company in 1615, which shows that Ralph was the son of Robert White of South Weald in Essex. His father<a  href="/article/london-wills.php/#robwhite"> Robert</a> died testate.
+
+<img class="img-fluid" src="/img/wm_wht_appr2.png" alt=""  />
+
+The earliest record located for the family of South Weald is that of the burial record in latin in 1540 of Laurence Whyet son of Robert Whyett of Brentwood, Brentwood being a village in the parish of South Weald. There is another similar record in 1542 of a baptism of Laurence Whiet son of Robert Whyett, evidently given the same name as his deceased sibling.
+
+<img src="http://jimserver.net/graphics/lawrence_wht_burial.png" alt="register" />
+
+A Robert White of South Weald married Christian Bocher of Great Bursted, widow in 1550(London Marriage Licenses), possibly the same <a href="/article/london-wills.php/#robwhite_1563">Robert White</a> who was buried in South Weald on 20 April 1563 and left a will recorded 20 July 1563. He names only one underage daughter, so it is uncertain if he was the same as the parent of Laurence.
+
+The South Weald register has record of John White married to Anne Collet on 27 Sept 1563 and the baptisms to John of sons Robert(1569) and Samuel(1567). The will of<a href="/article/london-wills.php/#john_white_1612"> John White</a> of Shenfield, dated 1612, shows that he was the father of Robert White and grandfather of Ralph White, grocer.
+
+The register of Willingale Doe shows that Humffray Whighte &amp; Elizabeth Bruce were married the sixteenth day of October in 1586. Baptisms of children of Humfray White at Willingale Doe were Richard(1589), Stephan(1592), Humffray(1593), Lawrence(1596), Thomas(1598/9), Robert(1600), Elizabeth(1603), and Christopher(1608). Also recorded were baptisms to the families of Thomas White (1570s) and Richard White (1580s). Umphry White was buried at the nearby parish of Roxwell on 10 July 1632.
+
+A search was made in Essex for any contradictory evidence that Thomas White of Willingale Doe might not have been the same who came to Virginia. The register of Willingale Doe records the burial of a Thomas Whyt on 28 November 1623, and the burial in 1617 of an Alice White wife of Thomas. Most likely the burial in 1623 refers to the older married Thomas who was the father recorded in several baptisms in the 1570s. The IGI has record of a christening of Joan Whyte, daughter of Thomas Whyte and Bridget on 26 Oct. 1629, but reference to the original record shows it to be Thomas Wryte. The search located some records in the nearby parish of Leaden Roding that might be a related family. There is a will for<a href="/article/london-wills.php/#richardwhite_1642"> Richard White</a> in the adjacent parish of Willingale Spain, dated 1642. The will of “Richard White of Willingale Spayne in the county of Essex carpenter” mentions 2 parcels of land in Willingale Spain, one of which appears in the 1625/26 probate of the will of a Thomas White of Leaden Roding where he bequests “unto Richard White my youngest sonne all that my copyhold or customary messuage or tenement in Willingale Spain.” However nothing was found to exclude the possibility that Thomas of Willingale Doe and Thomas of Virginia were the same person.
+
+The Whites of South Weald and of Willingale Doe both used the names Robert, Laurence, and Thomas. The parish of Shellow Bowells, adjacent to Willingale Doe, has record of a Ralph White, son of John, baptized 1562. The register of Great Warley, Essex, has the marriage in 1629 of Lawrence White and Jone, and 1647 the christening of Humphry White, son of Lawrence and Johane. These 2 records show that probably Lawrence, son of Humfry moved from Willingale to Great Warley, a parish adjacent to South Weald, by 1629.
+
+Assuming that Thomas White of Lower Norfolk was in some way related to Ralph White of London and that Ralph White was the same as grocer of Stepney, then the final part of the proof that Thomas White was possibly born in Willingale Doe is that the families of South Weald and Willingale Dow are related. The similarity of names and proximity of these families shows it is likely. However the following record, dated 10 July 1498, provides a possible explanation of how they are related.
+<blockquote>Conditional surrender (mortgage) by John Smyth de Hoton' to the use of Robert WHITE and Johanna the daughter of the same Robert and Johanna lately his wife and formerly the wife of Thomas Smyth, for 23s. 4d.
+
+Tenement and lands called Newmans,
+copyhold of the manor of Willingale Doe
+
+(Essex record office D/DQ 90/3)</blockquote>
+The Hoton mentioned is Hutton, the parish next to Shenfield, hence one over (1-2 miles) from South Weald. Possibly the Robert White mentioned lived in South Weald and received this land and moved to Willingale Doe. Possibly he could have been the grandfather or gr-grandfather of Humphrey White. Willingale Doe parish registers start in 1570, so it is likely the Humphrey was baptized before that time. This idea is somewhat supported by the fact that the subsidy of 1327 has no entry for the name of White in Willingale Doe, but a Henry White living in South Weald.
+
+
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
