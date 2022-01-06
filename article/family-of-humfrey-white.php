c05782f988f9d14b315e74c6feec179689620344
jim <jbwhite2@ncsu.edu>
2022-01-06 13:02:32 -0500

    articles

---
+++ article/family-of-humfrey-white.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+    <h2>Family Of Humfrey White</h2>
+
+<h5>The following is what I can find out from parish records and a few court records about the family of Humffrey White of Willingale Doe. There were no estate or land records located.</h5>
+<p>
+1. Humffrey White, married at Willingale Doe, Oct. 16 1586, Elizabeth Bruce. He was buried at the adjacent parish of Roxwell on 10 July 1632. Children of Humffrey:<br>
+2. i. Edward, baptized 11 Sept. 1588, at Willingale Doe.<br>
+3. ii. Richard, baptized 25 Jan. 1589/90 at Willingale Doe.<br>
+iii. Stephan, baptized 16 May 1592 at Willingale Doe.<br>
+4. iv. Humffrey, baptized 17 March 1593/4 at Willingale Doe.<br>
+5. v. Lawrence, baptized 24 Oct. 1596 at Willingale Doe.<br>
+6 vi. Thomas, baptized 18 Feb. 15 1598/9 at Willingale Doe.<br>
+vii. Robert, baptized 18 Jan 1600/1 at Willingale Doe.<br>
+viii. Elizabeth, baptized 27 March 1603 at Willingale Doe.<br>
+7. ix. Christopher, baptized 17 April 1607 at Willingale Doe.<br>
+<br>
+2. Edward White  moved to the parish of Great Waltham, several miles northeast of Willingale. He married Sara Rice daughter of Alexander Rice of Willingale Spaine, as shown by his will of 1618. Children of Edward and Sara White:<br>
+8. i. Edward, baptized 15 May 1614 at Great Waltham.<br>
+ii. Grace, baptized 6 Oct. 1616 at Great Waltham.<br>
+iii. Thomas, baptized 7 Dec. 1620 at Great Waltham.<br>
+iv. John, baptized 12 Aug. 1624 at Great Waltham.<br>
+v. Jone, baptized 12 Aug. 1624 at Great Waltham.<br>
+<br>
+3. Richard White, probably the same who married Agnes Weale 29 Sept. 1617  at Roxwell.<br>
+<br>
+4. Humffrey White evidently moved to Great Warley, adjacent and south of South Weald around the same time as his brother, Lawrence. He appears in the parish register there one time as follows: Denbigh the sone of Humphrey White and Phillip(a) his wife was baptized the 27th of December 1628.<br>
+<br>
+5. Lawrence White evidently moved to Great Warley. His first wife was named Margery who was buried 18 April 1629 at Great Warley. He married second at Great Warley 13 Nov. 1729 Jone Nobe. Children of Lawrence White by Margery:<br>
+i. Elizabeth, baptized 18 May 1628 at Great Warley.<br>
+Children by Jone:<br>
+ii. Mary, baptized 26 June 1631 at Great Warley.<br>
+iii. William, baptized 9 Aug 1635 at Great Warley.<br>
+iv. Daniel, baptized 29 March 1640 at Great Warley.<br>
+v. Thomas, baptized 4 April 1641 at Great Warley.<br>
+vi. Lawrence, baptized 16 June 1644 at Great Warley.<br>
+vii. Humphrey, baptized 23 Dec. 1647 at Great Warley.<br>
+<br>
+6. Thomas White, presumably in Virginia by 10 March 1637 when he is used as a headright by Thomas Sawyer for a land grant in Lower Norfolk County.<br>
+<br>
+7. Christopher White married at the adjacent parish of Roxwell on 22 June 1629 Judith Hall of Woodham Walter, Essex. Christopher appears in court records 3 times around 1640. Court of 22 Oct 1641 has "Edward Bowen sen., yeoman., Edward Bowen jun., shopkeeper, both of Roxwell; Edward senior to keep the peace to Christr. White of Writtle labourer." Record of 26 October 1638 has "Christopher White of Writtle husbandman and Edward White of Great Waltham collar-maker; Christopher to keep the peace to Robert Kitchin of Writtle," and record of 22 Oct.1641 has "Recognizance of Christr. White and Edward White of GreatWaltham husbandman; Christr. to keep the peace to Edward White senior." Children of Christopher White:<br>
+i. Judith, baptized 19 May 1630 at Roxwell.<br>
+<br>
+8. Edward White married 25 April 1632 at Great Waltham Sarah Crosier. Children of Edward and Sarah:<br>
+i. Sarah, baptized 25 Oct. 1633 at Great Waltham.<br>
+ii. John, baptized 5 Dec. 1637 at Great Waltham.<br>
+
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
