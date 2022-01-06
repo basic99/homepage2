0283ffcd332a248f3ef600413acaada752d8261f
jim <jbwhite2@ncsu.edu>
2022-01-06 13:34:51 -0500

    articles

--- article/english-origins-of-ward-family-of-virginia.php
+++ article/english-origins-of-ward-family-of-virginia.php
@@ -31,24 +31,24 @@ include "../menu.php";
   <div class="col-md-8">
     <h2>English Origins Of Ward Family Of Virginia</h2>
 
-<p>The immigrant ancestor of the Ward family of Henrico County, Virginia, is stated to be Seth Ward in Ward Family [William & Mary Quartley, v. 27, pp. 185-199], and part 2. The book Sir Ellis Hicks, Captain John Ward by Eugene Hicks mentions John Ward of Virginia as possible father of Seth, but both sources identify Seth Ward of Virginia as father of Richard Ward of Virginia.
+<p>The immigrant ancestor of the Ward family of Henrico County, Virginia, is stated to be Seth Ward in<a href="http://www.jstor.org/stable/1916204"> Ward Family</a> [William &amp; Mary Quartley, v. 27, pp. 185-199], and <a href="http://www.jstor.org/stable/1916294">part 2</a>. The book Sir Ellis Hicks, Captain John Ward by Eugene Hicks mentions John Ward of Virginia as possible father of Seth, but both sources identify Seth Ward of Virginia as father of Richard Ward of Virginia.
 
 <p>Seth Ward lived in Henrico County by 1632 when he was granted the plantation of Varina. He died after 1650 without a will or otherwise identifying any descendants.
 
 <p>Richard Ward was granted 100 acres in Varina in 1637 including 50 acres for his own personal adventure. Richard Ward left a will in Henrico County, dated 1682, naming children Seth, Richard, Edward, and Elizabeth.
 
-<p>Richard’s son Edward lived in Henrico County until May 1708 when he is recorded in a suit as “being privately departed the county.” He was evidently the same Edward Ward who names his son Enoch Ward in 1712 in Craven County, NC, court records [Haun, Craven NC Court Minutes, 1712-1715, p. 1].
+<p>Richard's son Edward lived in Henrico County until May 1708 when he is recorded in a suit as "being privately departed the county." He was evidently the same Edward Ward who names his son Enoch Ward in 1712 in Craven County, NC, court records [Haun, Craven NC Court Minutes, 1712-1715, p. 1].
 
 <p>Considering the onomastic naming patterns here we can make a good guess as to the English origins of the Ward Family.
 
 <p>Seth Ward, son of Enos Ward, baptized 15 March, 1606, Litlington, Cambridgeshire<br>
 Richard Ward, son of Enoch Ward, baptized 1 Oct 1609, Litlington, Cambridgeshire
 
-<p>The records show that Richard Ward was not Seth Ward’s son but his brother.
+<p>The records show that Richard Ward was not Seth Ward's son but his brother.
 
-<p>Transcription of the will of Seth Ward of Abbington, probable grandfather of Richard and Seth of Virginia. In it he names his son Enosh or Enos, which is different than the name seen in Virginia, which is Enoch. However, I still think that the names are similar enough to think that Enoch of North Carolina was named for Enosh of England.
+<p>Transcription of the will of<a title="Seth Ward Will" href="http://jimserver.net/seth-ward-will/"> Seth Ward</a> of Abbington, probable grandfather of Richard and Seth of Virginia. In it he names his son Enosh or Enos, which is different than the name seen in Virginia, which is Enoch. However, I still think that the names are similar enough to think that Enoch of North Carolina was named for Enosh of England.
 
-<p>Benjamin Ward of Granville County, was a descendant of Richard Ward the immigrant, but his exact line is still  uncertain. The blog Ancestors of Virginia has some records that could help making this connection.
+<p>Benjamin Ward of Granville County, was a descendant of Richard Ward the immigrant, but his exact line is still  uncertain. The blog <a href="http://ancestorsofvirginia.blogspot.com/2011/07/richarad-ward-1692-1761-of-henrico.html">Ancestors of Virginia</a> has some records that could help making this connection.
 
 </main>
 
