cdbb99c35add7f0dcb8d166ac1a25fd86397f7ef
jim <jbwhite2@ncsu.edu>
2022-01-06 20:09:40 -0500

    articles

--- article/essex-connections.php
+++ article/essex-connections.php
@@ -29,7 +29,103 @@ include "../menu.php";
 <div class="row g-5 bg-light rounded">
   <div class="col-md-2"></div>
   <div class="col-md-8">
-    <h2>Home</h2>
+    <h2>Essex Connections</h2>
+
+    This article continues my discussion of the possible Essex origins of Thomas White with further analysis of early Essex records.
+
+The 1524 Lay Subsidy lists taxables by parish. The Whites that appear in the parish of South Weald or near to it are:
+
+Robert Whyt - South Weald
+Robert Whyt - Brentwood
+Symond Whyt - Brentwood
+John Whyte - Brentwood
+Thomas Whyght - Shenfield
+William Whyte - Shenfield
+Richard Whyghtt - Hutton
+
+All of these with the exception of Richard White of Hutton could have been relatives of the family of Ralph White. The family of Richard White were gentry, having moved to Hutton from Hampshire (the Humfrey listed in this record is not Humfrey of Willingale Doe).
+
+<img class="alignnone" src="/img/white_hutton.png" alt="White of Hutton" width="828" height="484" />
+
+Aside from the subsidy lists,  the first records in South Weald of the Whites are in the parish register - the baptisms of Lawrence and Benet White to Robert Whyte in the early 1540s. The next record is the 1563 will of<a href="/article/london-wills/#robwhite_1563"> Robert Whyghte</a> of South Weald. The witnesses to his will were John Grene, Nicholas Carvell vicar of South Weald, Larence Grene, Giles Greene, and John Payne.
+
+The 1612 will of <a href="/article/london-wills/#john_white_1612">John White</a> of Shenfield shows he was the father of Robert White, father of Ralph. There was also a suit in <a href="/article/john-anne-pond-v-robert-white/">chancery </a>concerning his estate. We know that because  the land that he bequeathed to his son Robert in Shenfield, Railey &amp; Hockley was also mentioned in Robert's will. He named his 3 children Samuel, Robert, and Ann. The South Weald register has records of baptism in 1567 of Samuell White son to John white and in 1569 of Robert White son to John White and in 1593 burial of Benet Whyte daugthter of John. The witnesses to his will included John Wright of Wright Bridge and Richard Wright. The 1633 will of <a href="/article/london-wills/#robwhite">Robert White</a> of South Weald, father of Ralph, mentioned a daughter Bennett White. The use of the name Benet White over several generations shows that the family of Ralph White lived in South Weald from at least 1540.
+
+The pedigree of the Wright family who also lived in South Weald has many of the names mentioned above.
+
+<img class="alignnone" src="/img/wright_london.png" alt="Wright of London" width="684" height="877" />
+
+Wrights Bridge was in South Weald. The marriage of John Wright of Wrights Bridge to Bennet Greene can be dated to around 1590 by the university records of Laurence and Nathaniel. This is 50 years after the names Benet and Laurence appeared in the White family, but there is clearly a connection, probably through the Greene family. The Wrights of South Weald were the ancestors of <a href="http://www.wright-brothers.org/Information_Desk/Just_the_Facts/Wright_Family/Wright_Genealogy/Wright_Genealogy.htm">Wilbur and  Orville</a>.
+
+Robert White in his 1633 will named a daughter Dorothy Wright. The will of <a href="/article/london-wills/#ralphwhite">Ralph White</a> was witnessed by a Nathaniel Wright. Appenticeship records for the Grocers Company show that Nathaniel Wright, son of Edward Wright, citizen, was apprenticed to Ralph White in 1656. The Stepney parish register has records of Nathaniell Wright, son of Edward, christened 12 Jan 1642, and Edward Wright, son of Edward and Dorothy Wright, christened in 1638. Finally, the parish register of Barking, Essex (between South Weald and Stepney), has record of marriage in 1628 of Edward Wright and Dorothy White. It appears that Nathaniel Wright who witnessed the will of Ralph White was his nephew, and almost certainly related to the Wrights of Wright Bridge. Another <a href="http://marinelives-tools.wikispot.org/PROB_11/274/558_Will_of_Nathan_Wright_of_Saint_Olave_Hart_Street%2C_City_of_London_27_April_1658">Nathaniel Wright</a>, a cousin of the Wright Bridge Wrights was a successful overseas trader in the middle 1600s, which makes it more likely that Ralph White was himself an overseas trader and was the one listed in Lower Norfolk records.
+
+The first record related to the Whites and Willingale Doe is a <a title="Essex Manor Records" href="/article/essex-manor-records/">1498 deed</a> where John Smyth of Hutton deeded land to Robert White and his daughter Joan in Willingale Doe called Newmans. By 1532 this land and a parcel named Gowers were in possession of Thomas Barnarde &amp; Margaret his wife, possibly heirs of Joan White, although there is no proof. Edward Barnard of Willingale Doe in his will dated 1600 mentioned  the land Gowers in his will so it is possible that descendants of the heirs of Joan White continued to live in Willingale Doe until that time.
+
+The 1571 will of John Smyth of Hutton (Hoton is an old name for Hutton) witnessed by Thomas White and the 1584 will of <a href="/article/london-wills/#thomas_white_1584">Thomas White</a> of Hutton witnessed by Samuel Smyth seem to show that relatives of those in this early deed still lived in Hutton. Aside from naming his children Thomas mentioned a lease to William Whyte of Shenfield and Sir George Whyte Esquire. George White was a son of Richard White of Hutton and probably not related, as there is no mention of a Thomas White in his pedigree at this time. The names Thomas and William in Shenfield in the lay subsidy list could be this same Thomas and William or an ancestor.
+
+The 1524 lay subsidy list has these listed with surname White living in or near to Willingale Doe.
+
+Humfrey Whyte - Shelley
+John Whyte - Good Easter
+John Whyte - Willingale Spain
+William Whyte - High Easter
+
+Two things are immediately obvious. First, at this time there were no taxables named White living in Willingale Doe, although the list would not include the working poor. That raises the question whether Robert White moved to Willingale Doe and left male descendants. Possibly the others listed in the area are his descendants but there is no way of knowing.
+
+The other noticable person in the list is Humfrey Whyte who lived in Shelley which is one parish over from Willingale Doe (1-2 miles). Shelley lies more or less between South Weald and Willingale Doe, so whether or not he was a descendant of the 1498 grantee he could have been related to the Whites in the South Weald area. The entry in the  original <a href="http://bit.ly/1SQU9uQ">pipe roll</a> is shown.
+
+<a href="/img/shelley_subsidy.jpg"><img class="aligncenter" src="/img/shelley_subsidy.jpg" alt="pipe rolls" width="2268" height="1408" /></a>
+
+The names on the list are William Stane, William Peg, John Field, Thomas Field, William Andrew, John Fuller, --- Hill, William Magot, Humfrey Whyte and William Crowe.  Humfrey was not as unusual name at this time (20th according to a <a href="http://www.s-gabriel.org/names/christian/fairnames/givennames.html">list</a> published by The Society for Creative Anachronism), but I still think that this Humfrey might have been the father or grandfather of Humfrey of Willingale Doe. Note that Humfrey White, younger  brother of <a href="http://www.historyofparliamentonline.org/volume/1509-1558/member/white-george-1530-84">George White</a>, was probably born after 1530, so he would not have been this Humfrey.
+
+<img class="alignnone" src="/img/greene_essex.png" alt="Greene of Essex" width="840" height="594" />
+
+In 1582 John and Catherine (Wright) Greene of Navestock purchased the manor of <a href="http://www.british-history.ac.uk/vch/essex/vol4/pp203-204#anchorn35">Shelley</a> - Navestock sharing a border with the north side of South Weald.
+
+Robert White of South Weald named 2 children Benet and Lawrence in the 1540s, and Robert White of South Weald the testator of 1563 had his will witnessed by John Greene, Lawrence Greene, and Giles Greene. A search of the 1524/5 lay subsidy rolls for the name Greene in the area between South Weald and Shelley shows these names.
+
+John Grene - Blackmore
+Richard Grene Snr - Navestock
+Richard Grene Jnr - Navestock
+Thomas Grene - Stanford Rivers
+
+The will of Thomas Greene, cutler, made bequests of land in Navestock and Stanford Rivers, so those 3 entries must all relate to the same family. The one from Blackmore might be a different family. Since there were no entries in South Weald and the Greenes of Navestock lived in the area by 1625 it is probable that the Whites of South Weald and the Greenes of Navestock and Stanford Rivers were related.
+
+Lawrence Greene, grocer, in records of 1618/1620/1624 was one of the “Adventurers to Virginia” investing 37 pounds 10 shillings [Records of the Virginia Company, ed. Susan Kingsbury]. In 1615 a petition of the parishioners of St. Stephens Walbrook included Reginald Greene, cutler, and Lawrence Greene, grocer [Trans. London &amp; Middlesex Arch. Soc, v.5, p. 338]. The wills of  <a href="/article/london-wills/#reg_greene">Reginald Greene</a> Citizen and Cutler of London, dated 1624, and Thomas Greene, cutler of London, dated 1616 [PCC] show the connection between the Greenes of Navestock and Shelley and Lawrence Greene, grocer. In the will of Reginald Greene he mentioned his brother Lawrence Greene and “Edward Greene son of my brother Robert Greene deceased student in Cambridge.” He also left five pounds to the poor of the parish of Navestock in Essex. The will of Thomas Greene, cutler,  shows that Lawrence Greene, grocer, was his former wife’s brother, and that Thomas Greene was born in Navestock and Reginald was his brother. He also mentioned the parish of Saint Magnus the Martyr (London) as did the will of Lawrence Greene, cutler, dated 1580 [PCC]. The following parish records are useful for constructing the Greene tree:
+
+St. Magnus the Martyr (start 1557):
+1561 - Jane, d. Lawrence Greene christened
+1562, 1563, 1565 - more children of Lawrence Greene
+1573 - Elizabeth, d. Lawrence Greene christened
+1575 - Agnes, d. Lawrence Greene christened
+5 April 1570 - Thomas Greene and Margaret Greene married
+2 Nov 1572 - Lawrence Greene and Agnes Elsam married
+21 Sept 1581 - John Spencer and Margaret Greene, widow, married
+25 Oct 1580 - Lawrence Greene buried
+
+Saint Stephen's Walbrook (start 1557)
+19 July 1585 Renold Greene and Christian Spencer married
+3 Apr 1597 Lawrence Greene s. Lawrence Greene christened
+29 Nov 1598 Margeret Greene d. Thomas Greene christened
+10 July 1600 Bennite Greene d. Lawrence Greene christened
+6 Jan 1601/2 Henry Grene s. Laurance Greene christened
+31 Aug 1603 John Greene s. Laurance Greene, marchant, christened
+9 July 1616 Richard Luke of St. Johns in Walbrooke and Mary Greene, of this parish, widow, married
+30 Nove 1620 Edward Brumley and Bennet Greene married
+
+The christening of Lawrence Greene, grocer was not located in records of St. Magnus. Since his sister Margeret was married to Thomas Greene in 1570, then it seems that he was born before the register starts and was the same who was married in 1572. The burial date for Lawrence Greene of 1580 and marriage of Margaret Greene widow in 1581 matches the 1580 will of Lawrence Greene[PCC], citizen and cutler, who named his wife Margaret, sons Lawrence, Thomas and John and 4 unamed daughters.
+
+&nbsp;
+
+<a href="/img/greene_tree.png"><img class="aligncenter" src="/img/greene_tree.png" alt="Greene tree" width="1106" height="607" /></a>
+
+The list of wardens (governing members of a company) of the Grocers Company list Laurence Greene in 1614 and as nominated but excused in 1621. The connections with overseas trade and the Grocers Company and Ralph White’s membership in the company make his involvement in overseas trade seem more likely. Also, Thomas Alexander a neighbor of the White family in Lower Norfolk county was a member of the Grocers Company.
+<blockquote>Petition of Lawrence Greene merchant, to the Lords of the Privy Council, October 7, 1641 – Being a trader to Virginia and having twenty four servants there and twenty others to go this present voyage in the Mayflower prays for a warrant for transporting said twenty passengers and provisions in the annexed schedule free of custom &amp; other duties. [Virg. Hist. Mag. v. 17, p. 17]</blockquote>
+This record probably refers to his son, <a href="/article/london-wills/#lawrence_greene_1650">Lawrence Greene</a>, merchant. born 1597, who left a will dated 1650. As he names his sister Bennet Brumley, he was the same baptised at Saint Stephens. He named his mother Margaret, so it appears that Lawrence Green, grocer was married second to Margaret.
+
+The entry for Edward Greene in the university database indicates that he was probably the same who was rector of Shelley from 1628-50-. Thomas White's father Humfrey died in 1632, and as a younger son he would have had little inheritance. It is easy to imagine him attending church at Shelley at this time and listening to Edward Greene, nephew of a contributor to the Virginia Company, talk of going to the new world and deciding to go. Perhaps he was one of the 24 servants that Lawrence Greene transported to Virginia before 1641.
+
+A search of Virginia records for Lawrence Greene found no records in the seventeenth century. The earliest record was as a headright in 1714. The land grant to Richard Flewellin was for 278 acres in Charles City County adjacent Mr. Drury Stith and Wm. Featherstone. The list of headrights in addition to Lawrence Greene were Stephen Noel, John Pain, John Lett, Daniel Collings, and John Hammond. Many of these names are familiar to this research, e.g. the previously mentioned connection with the Lewellin family, the headright of Ralph White to John Stith, the marriage of Jane Greene to (Raph?) Fetherstone, the John Payne who was witness to the will of Robert White in 1563 along with Lawrence Greene, and lastly John Hammond, the same last name as Christopher Hammond who was a headright with Thomas White in Lower Nofolk.
 
 
 
