9b3cee6de545d18821e1c22046c09af61717406e
jim <jbwhite2@ncsu.edu>
2022-01-06 20:00:53 -0500

    articles

--- article/the-llewelen-connection.php
+++ article/the-llewelen-connection.php
@@ -29,7 +29,47 @@ include "../menu.php";
 <div class="row g-5 bg-light rounded">
   <div class="col-md-2"></div>
   <div class="col-md-8">
-    <h2>Home</h2>
+    <h2>The Llewelen Connection</h2>
+
+    <p>There are records in Isle of Wight County, Virginia, dated 9 Nov. 1664 and 5 Jan. 1664/5 concerning the estate of Thomas Llewellin of Isle of Wight. His daughter, Eady, married 'John White of Southerne Branch of Elizabeth River in the County of Lower Norfolk Yeoman' who was suing for property from the estate [Brayton, W&amp;DB 1, f. 22, 26]. Eady (Llewellen) White evidently died between this time and 21 July 1665 when Paul Luke was granted land formerly owned by Thomas Luellin and the grant states that Thomas Luellin died without issue now living [Nugent, v.1, p. 462]. The John White mentioned is probably my ancestor John White, son of Thomas, although his children were evidently from a later wife.
+
+<p>The earliest record that probably refers to Thomas Llewelen is the will of 'John Ring of London, Yoeman, being bound for Virginia,' dated 31 Aug. 1636. In it he mentions 'Thomas Lluellinge, livinge att the Pottashe quarter in Virginia,' and 'my brother Matthew Ringe, plummer of the Strand, London'.
+
+<figure class="figure">
+<img class="img-fluid"  src="/img/john_ring.png" alt="ring will" />
+</figure>
+
+<p>Reference to land grants show Pottashe quarter to be in Warwick County, just across the <a href="/img/james.png">James River</a> from Isle of Wight County. Headrights for Tho. Flewellin and Ann Flewellin were used by Henry Neale in 1643 [Nugent, v.1, p. 146]. The will of Robert Watson of Isle of Wight, 1651, states that Ann Watson, widow of Robert, desired Toby Hurst to give a calf to the youngest daughter of Thomas Fluellen [Brayton, WB A, f. 29]. Between 1636 and 1651 Thomas Llewelen moved across the James River to Isle of Wight County. Since his wife when he died was named Sarah, the most likely identification of the Ann Flewellin headright is that she was the wife of Robert Watson and the sister or cousin of Thomas.
+
+<p>The wills of '<a title="London Wills" href="/article/london-wills.php#mathiasring">Mathias Ring</a> of the parish of St. Martin in the fields in the county of Midd Cittizen and plumber of London' and '<a title="London Wills" href="/article/london-wills.php#andrewring">Andrew Ringe</a> citizen and draper of London,' father of Matthew and John Ring, were found. Matthew Ring named his wife Penticost Ring, and daughters Elizabeth and Susanna. The will of Penticost Ring mentions that her daughter Susan was married to a Llewelen. The Visitation of Northampronshire 1681 has an entry for Llewelen of Tansor.
+<figure class="figure">
+<img class="img-fluid" src="/img/lewellin.png" alt="visitation" />
+</figure>
+
+This shows that Matthew Ring's daughter was married to David Llewelen, son of David Llewelen of Windsor, housekeeper to King Charles. The chance that the Ring family connection to Thomas Llewelen and David Llewelen was to two different families seems remote, so Thomas Llewelen of Isle of Wight must have been some relation to the Llewelen family that was part of the royal household.
+
+Daniel Llewellyn was in Virginia by 1633 [Dorman, Adventurers of Purse and Person, Price-Llewellyn]. There is no evidence in Virginia records that he was related to Thomas Llewelen except of course they were both in Virginia. He married Ann Hallom, the widow of Robert Hallom whose family was from Essex. He died and was buried in Chelmsford, Essex, 13 Feb. 1663, his entry in the parish register reading 'Daniell Llewellen of Chelmsford gent formerly a captaine in Virginia buried.' There were no entries in the register to indicate that he may have been from Chelmsford originally. In his will he mentioned land in Virginia and named as one of his executors Thomas Vervell of Roxwell, Essex, gent.
+<figure class="figure">
+<img class="img-fluid" src="/img/luellin_will2.jpg" alt="luellin will" />
+</figure>
+
+<a title="London Wills" href="http://jimserver.net/london-wills.php#varvell">Thomas Varvell </a>of Writtle in Essex made his will in 1688. In it he left to his daughters in law Sarah Goodlad of Stepney and Hannah Browne of St. Olave in Southwark his manor of Benedict Oats in Writtle and Roxwell. Admiralty court records show a case dated 1650 where Thomas Vervell and Robert Llewellin were joint owners of the Increase [Coldham, English Adventurers and Emigrants, p. 123]. The record shows that at the time Thomas Varvell was living in Ratcliffe, a village in Stepney. That he was the Thomas Varvell buried in Stepney in 1693 is evident from this record and that his daughter in law was living there.
+
+The witnesses to the will of Thomas Varvell were Humphrey Hill David Ring and Jeremi Rous Scrivener. Jeremy Rous of St. Olave's, Southwark, Scrivener, was witness to a marriage in 1682, so the will was probably written and witnessed while he was in Southwark, and David Ring, one of the witnesses probably lived there. Andrew Ring, father of John and Matthew Ring was married on 10 May 1604 to Joan Porey at St. George, Southwark. Possibly David Ring was related to John and Matthew, but his ancestry is not known.
+
+Robert Lewellin was one of 9 signers of the Petition of the Merchants and Traders to Virginia and the Rest of the English Plantations in America to his Highness’ Privy Council, Aug. 3, 1658 [Virg. Hist. Mag., v.18, p.158]. A Robert Lewellin of London Salter was named executor of the will of John Hatton filed in Maryland, dated 1654, and probated 1663. He had died by the date of probate. Records of the Salter Co of London show Robert Lewellin(1638), Thomas Lewellin(1639), and Richard Lewellin(1646) admitted as freemen. Possibly Thomas Lewellin, salter, was the same mentioned in the PCC will of Judith Flewellin wife of William Flewellin Cittizen and Tallow chandler dated 1646.
+
+Although it is possible that the connection of the White and Llewelen families occurred after the move to Virginia, I will consider records that might show that the connection was from Essex.
+
+The headright of Ralph White was used by John Stith for a grant of 550 acres in Charles City County in 1664 [Nugent, v.1, p.493]. Daniel Llewellyn, born 1647, and son of Daniel of Chelmsford married Jane Stith, daughter of John Stith [Dorman, Adventurers of Purse and Person, Price-Llewellyn]. This connection is important since the records of Ralph White in Virginia mention only that he was of London, not that he was grocer of Stepney. This record connects Ralph White of Virginia with the family of Daniel Llewellyn of Essex. Thomas Vervell of Roxwell mentioned in the will of Daniel Lluellin was from the same village where Humffray White was buried.
+
+Essex court records of 30 April 1579 show an order for Humphrey Flewellen of South Weald, husbandman, to appear. Llewelen/Flewellen being a Weslsh name was unusual in Essex. Other early records of the name living in Essex are the apprentice records of Richard Fluellyn, son of Hugh of Chigwell, 1642 and John Llewellen son of Jasper of Chigwell, 1656. Parish records for Chigwell exist, but are in poor condition and I was not able to find any records for this family in the register.
+
+The parish register of Bocking, Essex, has record of marriage of Thomas Lewellin and Margaret Neale on 24 May 1613. The record looks somewhat like Meale, but reference to the writing of November shows it is Neale.
+
+<img src="/img/lewellen_neale.png" alt="bocking register" />
+
+Other entries in the Bocking register of interest are the burial of Margeria Lewellin wife of Thomas Lewellin 22 Sept 1611, the burial of Thomas Lewellin son of Thomas Lewellin on 21 April 1616, and the marriage of Henricus Neale and Jana Throwgood on 20 Feb. 1631/2. Bocking baptismal records are missing from the year 1605. Evidently Thomas was married twice, first to Margeria and second to Margaret. The connection to the Neale family and the existence of a Henry Neale might show a connction of the Thomas Fluellin headright used by Henry Neale in 1643. If such is the case and Thomas Llewelen of Virginia was from Bocking, it raises the question of whether it was the elder, who since he was married first before 1611 was born say 1590, or if he might be a son named Thomas after the burial of the first in 1616, who then would have been born after 1617 and would have been no older than 19 when mentioned in the will of John Ring.
 
 
 
