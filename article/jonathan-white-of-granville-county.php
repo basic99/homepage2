54eb10c7ee7e7f1b0591e3738a52ad6c07821e9b
jim <jbwhite2@ncsu.edu>
2022-01-08 14:52:29 -0500

    article

---
+++ article/jonathan-white-of-granville-county.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+    <h2>Jonathan White Of Granville County</h2>
+
+<p>Jonathan White was an early settler of Granville County, N.C., a justice of the peace, and in 1748 was one of three commissioners appointed by the General Assembly to choose a location for the Granville County Courthouse. He was a member of the White family of the Southern Branch of the Elizabeth river in Norfolk County, Va. When he died in 1772 he was married to Sarah White. There is deed in Granville County in 1757 from Jonathan White and wife Faith to Ezekiel Fuller. This writer and others had previously identified this person as Jonathan White, Jr. However, a closer examination of the records has shown that a better explanation is that Jonathan White was married first to Faith, and second to Sarah. The following is his will, recorded in Granville County court in August 1772:
+
+<blockquote>I Jonathan White of the County of Granville give to my loving wife Sarah White two Negro girls, Rachael and Abbe, two feather beds and furniture, one bay mare and colt, and one Negro girl Nan during her life. I give to my son Jonathan White 140 acres of land where he now lives. I give to my son Burgess White 140 acres of land where he now lives. I give to my son William White 100 acres of land where he now lives. I give to my son Philemon White 100 acres of land where John Davis now lives, and I desire if my son Philemon die without marriage or lawful heir the 100 acres be sold and the money to be equally divided between all my children living, and it is my desire that all the remainder part of my estate be sold and the money equally divided between all my children, and at the death of my wife Sarah White I desire the Negro girl Nan to be sold and the money to be equally divided between all my children. I appoint my wife Sarah White and Thomas Bradford my executors February 28, 1772. Witnesses: Henery White, Burges White, Jonathan White</blockquote>
+
+<p>On 6 Dec. 1757 Jonathan White and Faith his wife sold Ezekiel Fuller 200 acres of land on both sides of Tabbs Creek in Granville County. A comparison of this 1757 deed with a deed dated 29 Nov. 1748 to Jonathan White, Esq., shows that they refer to the same parcel of land. Since this was the first land sold to Jonathan White in Granville County, and Jonathan, Jr., was still listed on the Granville County list of tithables with his father as late as 1755, then the Jonathan that sold the land in 1757 and whose wife was named Faith must have been the elder Jonathan White. This would seem to be true because since the title esquire must refer to Jonathan White (assumed to be the elder) who was a justice of the peace at the time. Also, Jonathan White, Jr., was married to Martha (___), not Faith, in 1777 when Jonathan and Martha White sold 140 acres in Granville County to Hugh Currin. The 140 acres would seem to be the same 140 acres given by his father 's will in 1772. Evidently, Jonathan White, Sr., was remarried to Sarah by 1764 when Jonathan White and his wife Sarah sold 200 acres in Granville County on 15 Aug 1764. The identity of Faith (___) White is not known.
+
+<p>The identity of Jonathan White 's widow Sarah can be discovered by later records referring to his bequest to his widow of the Negro Nan. First, we need to talk about the daughter Mary of Jonathan White. The will of Frederick Homes of Granville County, dated 29 Jan. 1749/50, named wife Mary, son John Homes, and daughter Priscilla. He appointed " Jonathan White my father-in-law and Richard Harris my brother-in-law my two executors and trustees. " The witnesses were John White and James Horton. Probably, father-in-law in this instance indicates that Mary Homes was the daughter of Jonathan White. In Granville County Court November 1763 Thomas Bradford returned the guardian account of the estate of Frederick Homes. It included payments for the schooling of John and Priscilla Homes. In Thomas Bradford 's will dated 22 March 1785 he named his wife Mary. Hence, it is likely that he married Mary (White) Homes and was guardian of her children by Frederick Homes. The identification of Thomas Bradford as Jonathan White 's son-in-law would be consistent with his being named an executor of Jonathan White 's estate. In his will Thomas Bradford made the following bequest; " I give to my son Philemon Bradford two parts of nine of a Negro woman named Nan, one part being given part, and the other bought of Philemon White which he is to have after the death of Sarah Rush." From this bequest and the will of Jonathan White two conclusions are evident. The first is that the widow of Jonathan White was remarried to a Mr. Rush, and the second is that Jonathan White had nine children. It is likely that Mary (White) (Homes) Bradford gave her part of the bequest in the will of Jonathan White to her husband Thomas Bradford.
+
+<p>The marriage bond in Bute County, N.C., dated 26 March 1773, proves that Sarah White was remarried to Benjamin Rush. Sarah Rush made her will in Franklin County, N.C., dated 2 Dec. 1790. In it she made bequests to her husband Benjamin Rush, brothers John, Moses, and Jacob Bledsoe, sisters Elizabeth Harril and Catherine Thornton, sister-in-law Jane Bledsoe, widow of George Bledsoe and her children, and the children of sister Anne Wooten, deceased. Notably lacking is any mention of the children of Jonathan White. No further mention was found if the Negro Nan. However, there was a bill in the General Assembly session of December 1791 to January 1792 to liberate Abislom and Rachel Spicer, slaves, the property of Benjamin and Sarah Rush. It is possible that Rachel Spicer was the same Negro Rachael that Sarah White received from the will of Jonathan White. Abraham Bledsoe made a will in Granville County, dated 15 March 1753, which named sons Isaac, Abraham, Thomas, Jacob, Moses, and Aaron and wife Sarah. The similarity of names in this will and the will of Sarah (Bledsoe) (White) Rush indicate that she was probably a daughter of Abraham Bledsoe. Abraham Bledsoe and Sarah his wife sold 400 acres in Spotsylvania County, Virginia, to Thomas Watts on 5 June 1732. Sometime after he sold his land in Spotsylvania County he moved to North Carolina, where on 10 Feb. 1742 he purchased 200 acres in Edgecombe County from William Person. Since the family of Abraham Bledsoe moved to North Carolina between 1732 and 1742, and some of the children of Jonathan White, especially Mary, were probably born before 1732, then these dates support the fact that Sarah White was the second wife of Jonathan White.
+
+<p>The Granville County tax lists from 1770 to 1784 show which of Jonathan White 's children continued to live in Granville County:<br>
+
+    1770-Jonathan White (7 tithables), William White (1), Burgess White (1), Jonathan White (1), John White (1), Henry White (1).<br>
+    1772-Henry White (1), Burgess White (1), Jonathan White (1), Philemon White (1), William White (1), Sarah White (4), John White (2)<br>
+    1774-Jonathan White(2), William White(1), William White(2), John White(1)<br>
+    1775-William White (2), Jonathan White (2), Burgess White (1), John White (1), William White (1)<br>
+    1784-Jonathan White (219 acres), Philemon White (0), Philemon White (156), William White (980), William White (295)
+
+<p>There were nine children of Jonathan White, probably all by his first wife Faith. Four sons are mentioned in his will, and two others, John and Henry, are identified by receiving legacies from the estate. Daughter Mary and possibly an unmarried daughter Fatey brings the total known or suspected children to 8 out of 9 indicated from the settlememt of Jonathan White 's estate. Issue: (1) Mary, born ca. 1730, married (1)Frederick Homes ca. 1746, married (2)Thomas Bradford. (2) John White, born ca. 1732, married Mary Bradford bef. 1763, died Chatham County, N.C. 1799. (3) Jonathan, born bef. 1735, married bef. 1777 Martha (___), died intestate or moved from Granville County between the census of 1790 and 1800. (4) William, born bef. 1739, married bef. 1773 Mary (___). (5) Henry, born bef. 1739, married Jemima Harris, died Granville County 1776. (6) Burgess, possibly moved to Davidson County, Tenn., before 1784 when a Burgess White was mentioned on a land grant. (7)Philemon, possibly the same Philemon who died in Granville County in 1803. (8) possibly, Fatey mentioned as Fatey White in the will of Robert Hicks in Granville County, and who was possibly named for her mother Faith White.
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
---
+++ article/manning-of-lower-norfolk.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+    <h2>Manning Of Lower Norfolk</h2>
+<p>Lower Norfolk Court, 16 June 1645. The court doth order that Thomas White and John Manning shall paye unto Arthur browne, merchant 554 lb tob due per bill by the 10 Oct next with 1 years forbearance of the said debt with court charges otherwyse execucon.
+<p>Ref: Walter, Lower Norfolk County, p. 172.
+
+<hr>
+
+<p>John Mannning, 200 acres, Lower Norfolk Co., 25 Oct. 1648. Upon the northward side of Gaythers Creek, being a branch of the Southward branch of Elizabeth River, running onto land of Marmaduke Merrington.
+<p>Ref: Nugent, Cavaliers and Pioneers, v. 1, p. 180.
+
+<hr>
+
+<p>Thomas White the elder aged fifty eight or thereabouts saith/ that being at my house John Manning his wife came over with him to my house/ for to tax Walter Costing my sonne Tho: concerning some gross aspertions/ wch he said he heard that my sonne Walter Costing should declare upon/ his wife concerning James Danby, he said ye he heard that they declared it/ to Ann Godby I hearing at ye same time, Ann Godby was at Francis/ Fleetwoods, went with my sonne Walter Costing Jno Manning his wife/ to Fran: Fleetwoods to cleare my son and Walter Costing of what John/ Manning had taxt them with, then Ann Godby cleared my sonne Walter/ Costin and this was ye words following wch I heard Jno Manning declare ye/ he heard my sonne Walter Costin say should say of his wife his ye coming/ home to his house he asked his children where their mother was/ who answered him she was gone to ye garden by ye tob house to gather/ hearbs to make a poultice for Besse Mr Davis maid hers burst, he going/ to ye tob house he found James Danby strugling with his wife there had/ one hand under her, with ye other hand kept James Danby from hers and/ wch ye John Manning down on his knees said for any things ye he knew/ there was how if a woman as any was in Virginia for he saw none/ hurt by him Jenny Wilkinson and Ann Godby Francis Fleetwood wife/ was in the house at ye same time.
+<p>Sworn unto in court 15th July 1657 the mark of
+<p>Thomas TW White
+<p>Ref: Lower Norfolk Book D, unnumbered
+
+<hr>
+
+<p>Lower Norfolk Court, 6 April 1660. The banns of Matrimony are sett up at y court door between Jno. Manning Lidia Richardson both of the parish of Elizabeth River.
+<p>Ref: Lower Norfolk Book E, p. 253.
+
+<hr>
+
+<p>To all Xryian people to whom this present writing shall come John Manning sendeth greeting in our Lord God Everlasting know ye that I the said John Manning of the country aforesaid in the county of Lower Norfolk my heires executo adm and assignes for divers good cause unto me paid before the sealing and delivery hereof by John Norgrane have given granted unto the said John Norgrane of the Island of Barbadoes his heires, executo and adm and assignes forever all my tract or devidend of land containing five hundred acres or thereabouts lying and being the county aforesaid wch said land butts and borders on the north side of Gaithers Creek in the Southern Branch of Elizabeth River in the country aforesaid and so extending itself to the tract or divend of land belonging to Marmaduke Marrington and Nicholas Robinson to have and to hold the said tract of of land as aforesaid being five hundred acres of thereabouts with all houses orchards profits advantages as fishng fouling hunting or any other advantage whatsoever standing lying or growing on the said land or any of . And further do sell for to the said Norgrane four milk cowes with the aforesaid land belong to the same the names of the said cowes are here under writen I the said John Manning my heires ex adm and assignes with Lydia my wife forever In witness where we hereunto set out hands and fixt our seals fifth day of March 1661
+<p>signed sealed        John IM Manning
+<p>delivered in the presence         Lydia LM Manning
+<p>of us Tho Whyt
+<p>Wm Etheridge
+<p>Nicholas Robinson
+<p>Ref: Lower Norfolk Book E, p. 39.
+
+<hr>
+
+<p>To all Xryian people to whom this present writing xhall come I John Manning of the Southern Branch of Elizabeth River send greeting Know you that I the sd. John Manning as well for the naturall love and affection I have and doe bear to Mary Hodges my grand child being the daughter of my daughter Mary Hodges and Roger Hodges her husband as unto her heyres and for divers other goood causes and considerations hereunto especially meaning have given and granted and by these presents doe give and grant and confirm unto the sd Mary Hodges my granddaughter and unto her heires executores adminstratores and assigne forever all that part or parcel of land lying in a Ridge ajoyning to the Swamp by estimation twenty acres be the same more or less lying and being in Julians Creek in Eliz. River and part of 300 acres which I the sd. John Manning formerly bought of one Thomas Whyte and whch said twenty acres of land is lately seated and a house built on the same by the said Roger Hodges father of the sd Mary together with free liberty for the sd Mary Hodges her heirs In witness whereof I have hereunto putt my hand seal this 2nd day of April in the 20 year of the Reign of our sovereign Lord Charles Anno Dom 1669
+<p>Signed sealed and devivered in presence of       John MI Manning
+<p>William Cole
+<p>Thomas Nash
+<p>Richard Taylor
+<p>Ref: Lower Norfolk Book E, p. 47.
+
+<hr>
+
+<p>Lower Norfolk Court, July, 1669. Ordered that Nicholas Robinson and Marmaduke Marrington apraise the estate of Jno. Manning decd. att any tyme when Capt. William Carver shall think fitt to give them [?]; he having [?] to take the inventory for to present the estate being very [indebted?]
+<p>Lower Norfolk Book E, f. 44.
+
+<hr>
+
+<p>Know all men by these presents that I Edward Norrington with the consent of my wife Lydia have alienated bargained and sould from me my heyres executors admin or assigns one deudent or tract of land where I now live being the land that my aforesd wife had for her thirds of her former husbands land John Manning decd lying and being in a creek commonly called or known by the name of Julians Creek together with all housing orchards farming and all other rights profitts whatsoever be me or my sd wifes now belonging or apertayning the present crop now on the ground excepted which I am to have the produce to ... and carry away att my owne discretion with out trouble hindrance or molestation and I doe further oblige my ... admin or assigns to the sd. John Manning his heirs that I and my aforesaid wife Lydia shall make him or that acknowledged court according to law between this and the last of december next and also make delivery of the premises always provided that I have liberty before or after to fetch and carry away my corn cattle whatsoever else is or may be myne in witness whereof I have hereunto sett my hand and seale 1672
+<p>Edward T Norrington
+<p>Lydia X Norrington
+<p>Ref: Lower Norfolk Book E, p. 136.
+
+<hr>
+
+<p>Lower Norfolk Court 15 Aug. 1672. Upon the request of Roger Hodges is is ordered that Edward Norrington and his wife be summoned to the next court to give an accompt of the orphans estate of Jno. Manning decd., and also to answer the contempt of Mr Portens warrant and the the sherif take good security of them for theire appearance.
+<p>Ref: Lower Norfolk Book E, f. 83.
+
+<hr>
+
+<p>Lower Norfolk Court, 15 April 1674. Judgement confessed by Thomas Manning who married the relict of John Watford for the payment of 418 lb of tob and cask to Mr. James Thelaball with cost asl ex being due by a bill from order the sd. Watfords hands.
+<p>Ref: Lower Norfolk Book E, f. 107a.
+
+<hr>
+
+<p>Lower Norfolk Court, 16 April 1674. Whereas in November court last itt was ordered that Edward Norrington father in law to Jno Manning should satisfie and pay unto John [Jacob?] the sd. Mannings master for entertaining the sd. Manning soe much of the act allow for entertayning for runaway servts. wch law the court does now apprehend was not intended for entertayning of the notion of the country (by their parents) who are not of bought servts. because relationship bound and therefore have ordered that the Norrington be acquitted from the part of that orders of November Court but he pay the cost of suite et exc.
+<p>Ref: Lower Norfolk Book. E, f. 108a.
+
+<hr>
+
+<p>Will of Margrett Taylor, d. 5 April 1679, r. 15 Aug. 1679. ... Brothers Roger Hodges, Jno. Manning, and Thomas Manning ... to be my overseers.
+<p>Ref: McIntosh, Lower Norfolk ?.., p. 68.
+
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
