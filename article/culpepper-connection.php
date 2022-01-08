ba53a6671c5f8d54bdcb644cf31b21100e5644f5
jim <jbwhite2@ncsu.edu>
2022-01-07 22:11:31 -0500

    article

--- article/culpepper-connection.php
+++ article/culpepper-connection.php
@@ -29,7 +29,31 @@ include "../menu.php";
 <div class="row g-5 bg-light rounded">
   <div class="col-md-2"></div>
   <div class="col-md-8">
-    <h2>Home</h2>
+    <h2>Culpepper Connection</h2>
+
+    <blockquote>That Essex baptismal record is important because it names the previously unidentified parents of John Culpepper of co. Essex:<br>
+
+"John, the sonne of Henry and Alice Culpepper was baptized December 23th (1645)"<br>
+
+This is <a href="http://www.culpepperconnections.com/ss/p38738.htm">John</a>. We can assume from the baptismal record that John's father, Henry, was probably born between 1605 and 1625.<br>
+
+The American Culpepper progenitor, Henry Culpepper of Lower Norfolk Co., VA, is shown in our records as "born say 1633, probably in England". I see nothing that would disqualify Henry of Lower Norfolk as a candidate for being the father of John of Essex. I see no other Henry Culpepers that I can't rule out for one reason or another.<br>
+
+In reviewing all of the above, we should keep in mind that through DNA testing we have established that Henry Culpepper of Lower Norfolk is not related to the more famous Culpepers of England.</blockquote>
+
+<p>The above is an edited email I received from Warren Culpepper when I sent him a copy of the quoted baptismal record from the parish register of Boreham in Essex. Warren has spent a lot of time researching the Culpeppers in England and America including DNA testing and founded the website <a href="http://www.culpepperconnections.com/index.html">Culpepper Connections!</a>
+
+<p>The first record of a connection of Henry Culpepper and the White family in Lower Norfolk County was in 1668 when Henry and John White both appeared on an estate record of Richard Russell. In 1670 John White was witness to a grant of right of attorney of <a href="http://jimserver.net/move-to-maryland-3/#culpepper1">Henry Culpepper</a> to his wife Elizabeth, and in 1671 John White sold his inheritance of 225 acres to <a href="http://jimserver.net/move-to-maryland-3/#culpepper2">Henry Culpepper</a>. The identity of Elizabeth Culpepper is not known but one possibility is that she was John White's sister Elizabeth.
+
+<p>Henry Culpepper was in Virgia by 1653 when he appeard as a headright[nugent, v1, p 242]. After that he appeared in several records in <a href="http://www.culpepperconnections.com/ss/p1.htm">Lancaster County</a> in 1658. He first appeared in Lower Norfolk in 1661/2 when his name was posted "giving notice of his intended voyage for England" [Brayton, Lower Norfolk bk d, f. 338a]. Assuming that he sailed to England he had returned by June 1665 [Brayton, Lower Norfolk bk d, f. 427a]. Shipping records have a Henry and John Culpepper sailing from London to New England in the summer of 1664 [Complete Book of Immigrants 1661-1699, p. 64].
+
+<p>The record of Henry sailing for England records 2 other people sailing at the next shipping - Mary Cornix and Peter White. The will of <a href="http://jimserver.net/london-wills/#gilbert_white">Gilbert White</a> of Boreham, dated 1612, mentions children Thomas, Peter, and Mary. Possibly this was the same Peter, however there was a Peter White who left a will in Ware in 1647 who might have been Peter of Boreham. The will of William Crowe of Boreham dated 1643 mentioned land lately purchased of Thomas and  Peter White, so they were both evidently living shortly before 1643.
+
+<p>If Henry of Lower Norfolk was the same as Henry father of John of Essex then one reason for his return voyage in 1661/2 might have been that he had close family still living in England in this case a son John. Possibly Henry and his son John sailed from London in 1664 to return to America, but John decided to return to England.
+
+<p>Between 1669 and 1678 John and Mary Culpepper had 5 children christened at Shellow Bowells. Due to the fact that Culpepper is an unusual name in Essex, this John was probably the son of Henry christened at Boreham in 1645. Boreham is about 10 miles to the east of the Willingales and Shellow Bowells.
+
+<p>These facts taken together add to the circumstantial evidence that Thomas White was originally from Willingale in Essex.
 
 
 
