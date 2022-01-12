864827bd9badea0a63a18c8e99a841d29bd85f85
jim <jbwhite2@ncsu.edu>
2022-01-11 22:53:43 -0500

    page

---
+++ article/stanley-cup-champions.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+    <h2>Stanley Cup Champions</h2>
+
+
+        <h2>June 19, 2006</h2>
+        <p><img src="/graphics/stanleycup01.jpg" alt="roll out the carpet"/><br />
+        <img src="/graphics/20hurricanes.3371.jpg" alt="Williams celebrates"/><br />
+        <img src="/graphics/cam_block.jpg" alt="Cam makes save"/><br />
+        <img src="/graphics/388-cup-getty-060619.jpg" width="388" height="230" alt="Rod kisses cup"/><br />
+        <img src="/graphics/06SCChampion_800.jpg" alt="championship banner"/></p>
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
