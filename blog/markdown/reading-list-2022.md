9079e46a474e0242e775ae86332864e49093c02c
jim <jbwhite2@ncsu.edu>
2022-01-22 12:35:19 -0500

    add page

--- blog/markdown/reading-list-2022.md
+++ blog/markdown/reading-list-2022.md
@@ -1,6 +1,30 @@
+Lion In the Bay
+
+January 22, 2022
+
+Stanley L Quick with Chipp Reid, *Lion in the Bay, The British Invasion of the Chesapeake 1813-1814* (Annapolis, 2015).
+
+***
+
+The Watchers
+
+January 22, 2022
+
+Stephen Alford, *The Watchers, A Secret History of the Reign of Elizabeth I* (London, 2012).
+
+***
+
+Agincourt
+
+January 22, 2022
+
+Juliet Barker, *Agincourt, Henry V and the Battle that Made England* (New York, 2005). As the title says this is a history of this famous battle.
+
+***
+
 The Brothers York
 
-January 21, 2033
+January 21, 2022
 
 Thomas Penn, *The Brothers York A Royal Tragedy*, (New York, 2019). A good history of the War of the Roses. The first history of this period I have read that I felt like I understood what happened.
 
---
+++ blog/reading-list-2022.php
@@ -0,0 +1,44 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <title>Jim White&lsquo;s Homepage</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link href="/css/homepage2.css" rel="stylesheet">
+
+
+    <style>
+    </style>
+
+  </head>
+  <body>
+
+<?php
+include "../menu.php";
+?>
+
+  <div  class='marspic'>
+  <img src='/img/mars_wp_hdr.png' />
+</div>
+<main class="container">
+<div class="row g-5 bg-light rounded">
+  <div class="col-md-2"></div>
+  <div class="col-md-8">
+    <?php include 'reading-list-2022.html';?>
+
+
+
+</main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+
+  </body>
+</html>
