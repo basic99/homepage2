ea96cdb1d5a422f840e3b3a0b5b169ee29df0aa4
jim <jbwhite2@ncsu.edu>
2024-12-24 10:59:24 -0500

    some js

--- js/compass-headings.js
+++ js/compass-headings.js
@@ -1,6 +1,13 @@
-
-
 $(document).ready(function() {
 
     console.log("ready");
+   const selectElem = document.getElementById("headings");
+
+    // When a new <option> is selected
+    selectElem.addEventListener("change", () => {
+        const index = selectElem.selectedIndex;
+        // Add that data to the <p>
+        let x = `selectedIndex: ${index}`;
+        console.log(x);
+    });
 })
