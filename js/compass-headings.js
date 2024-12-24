b02a13b9fe241d3afd2d6a1c5a04280bd9d3c0bc
jim <jbwhite2@ncsu.edu>
2024-12-24 13:04:57 -0500

    some js

--- js/compass-headings.js
+++ js/compass-headings.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+    var degrees = 0;
+
     console.log("ready");
     $("#degrees").html(`equivalent degrees: 0`);
    const selectElem = document.getElementById("headings");
@@ -10,7 +12,13 @@ $(document).ready(function() {
         // Add that data to the <p>
         let x = `selectedIndex: ${index}`;
         console.log(x);
-        let degrees = index * 11.25;
+        degrees = index * 11.25;
         $("#degrees").html(`equivalent degrees: ${degrees}`);
     });
+
+    $("button").click(function() {
+        console.log("clicked");
+        let radians =  (450 - parseInt(degrees)) % 360;
+        console.log(radians);
+    })
 })
