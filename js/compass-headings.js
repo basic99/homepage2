f2f5e559b14f5e75093399c2b63cde7ea9171f59
jim <jbwhite2@ncsu.edu>
2024-12-28 14:00:51 -0500

    dev compas

--- js/compass-headings.js
+++ js/compass-headings.js
@@ -9,6 +9,14 @@ function compass2trig(degrees) {
 
 }
 
+var points_list_fn = function(points_list) {
+    let text = '';
+    points_list.forEach((el) => {
+        text += `${el[0]}  ${el[1]}\n`
+    });
+    return text;
+}
+
 $(document).ready(function() {
 
     var degrees_compass = 0;
@@ -52,13 +60,7 @@ $(document).ready(function() {
         points_list.push([end_x.toFixed(3), end_y.toFixed(3)]);
         console.log(points_list);
 
-        points_list_fn = function(points_list) {
-            let text = '';
-            points_list.forEach((el) => {
-                text += `${el[0]}  ${el[1]}\n`
-            });
-            return text;
-        }
+
 
         let points_list_text = points_list_fn(points_list);
 
