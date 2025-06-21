86b116de367d410c5ce5a3c2560c2c92325ed617
jim <jbwhite2@ncsu.edu>
2025-06-21 15:32:31 -0400

    new page

--- reading-list.php
+++ reading-list.php
@@ -45,6 +45,7 @@ include "menu.php";
       <li class="list-group-item"><a href='blog/reading-list-2022.php'>reading list 2022</a></li>
       <li class="list-group-item"><a href='blog/reading-list-2023.php'>reading list 2023</a></li>
       <li class="list-group-item"><a href='blog/reading-list-2024.php'>reading list 2024</a></li>
+      <li class="list-group-item"><a href='blog/reading-list-2025.php'>reading list 2025</a></li>
     </ul>
 
 
