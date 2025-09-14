23d47a63dc567b085f166ff14c501f97f79a5536
jim <jbwhite2@ncsu.edu>
2025-09-14 18:15:18 -0400

    geneoalog page

--- article/madog_powys.php
+++ article/madog_powys.php
@@ -32,21 +32,41 @@ include "../menu.php";
     <h2>Welsh (Illegitimate) Royal Descent for Capt. William Peirce</h2>
 
 <ol>
- <li>Madog ap Maredudd (prince of Powys). He died 1160 and had twin illegitimate sons by Efa f. Eirian - Einion Efell of Cynllaith and Cynring Efell. [1]</li>
- <li>Einion Efell of Cynllaith, b. ca. 1130. [2]</li>
- <li>Rhun, b. ca. 1170 = Elizabeth d. John Strange of Knockin [3]</li>
- <li>Cuhelyn, living 1230 = Efa f. Gronwy [4]</li>
- <li>Ieuaf ap Cuhelyn = Efa f. Adda [5]</li>
- <li>Madog Goch, b. ca. 1270 = LLeucu f. Hywel Goch [6]</li>
- <li>Madog Cyffin = Tangw. f. Ien Foel [7]</li>
- <li>Ieuan Gethin ap Madog Cyffin = Margred f.Lln. [8]</li>
- <li>Gruffid = Margred ap rhys [9]</li>
- <li>Gwenhwyfar = Ieuan ap Gr. [10]</li>
- <li>Dafyyd Llwyd, b. ca. 1425 [11]</li>
- <li>Margaret vch. David b. ca. 1475, d. 1540 = John ap Llewellyn [12]</li>
- <li>Thomasin, b. ca. 1505, d. ca. 1540 = Piers Reynold</li>
- <li>Humfrey ap Piers Reynold, b. ca. 1535, d. 1598</li>
- <li>Capt William Peirce, b. ca. 1580, d. ca. 1647 in Virginia.</li>
+
+<li>Madog ap Maredudd (prince of Powys). He died 1160 and had twin illegitimate sons by Efa f. Eirian - Einion Efell of Cynllaith and Cynring Efell. [1]</li>
+<li>Einion Efell of Cynllaith, b. ca. 1130. [2]</li>
+<li>Rhun, b. ca. 1170 = Elizabeth d. John Strange of Knockin [3]</li>
+<li>Cuhelyn, living 1230 = Efa f. Gronwy [4]</li>
+<li>Ieuaf ap Cuhelyn = Efa f. Adda [5]</li>
+<li>Madog Goch, b. ca. 1270 = LLeucu f. Hywel Goch [6]</li>
+<li>Madog Cyffin, b. ca. 1300 = Tangw. f. Ien Foel [7]</li>
+<li>Ieuan Gethin ap Madog Cyffin, b. ca. 1330 = Margred f.Lln. [8]</li>
+<li>Gruffid, b. ca. 1370 = Margred ap rhys [9]</li>
+<li>Gwenhwyfar, b. ca. 1400 = Ieuan ap Gr. [10]</li>
+<li>Dafyyd Llwyd, b. ca. 1430 [11]</li>
+<li>Margaret vch. David b. ca. 1475, d. 1540 = John ap Llewellyn [12]</li>
+<li>Thomasin, b. ca. 1505, d. ca. 1540 = Piers Reynold</li>
+<li>Humfrey ap Piers Reynold, b. ca. 1535, d. 1598</li>
+<li>Capt William Peirce, b. ca. 1580, d. ca. 1647 in Virginia.</li>
+
+</ol>
+
+<h3>notes</h3>
+
+<ol>
+
+<li>https://en.wikipedia.org/wiki/Madog_ap_Maredudd; Dr P. C. Bartrum, Welsh Genealogies, Bleddyn ap Cynfyn 3, https://research.aber.ac.uk/en/datasets/bartrum-genealogical-project; Carl Boyer, Medieval Welsh Ancestors of Certain Americans, 3rd ed. (Santa Clarita, 2004), 14.</li>
+<li>Bartrum, Bleddyn ap Cynfyn 7; Boyer, 16.</li>
+<li>Bartrum Bleddyn ap Cynfyn 7; Boyer, 20.</li>
+<li>Bartrum Bleddyn ap Cynfyn 7; Boyer, 24.</li>
+<li>Bartrum, Bleddyn ap Cynfyn 8; Boyer, 26.</li>
+<li>Bartrum, Bleddyn ap Cynfyn 8; Boyer, 29.</li>
+<li>Bartrum, Bleddyn ap Cynfyn 8; Boyer, 31.</li>
+<li>Bartrum, Bleddyn ap Cynfyn 9; Boyer, 33.</li>
+<li>Bartrum, Bleddyn ap Cynfyn 10</li>
+<li>Bartrum, Bleddyn ap Cynfyn 10, Gwenwys 10.</li>
+<li>Bartrum, Gwenwys 10.</li>
+<li>James White, “The Possible Welsh Ancestry of Captain William Peirce of Jamestown and Mulberry Island, Virginia” (unpublished manuscript, 2021; updated 2025), http://jimserver.net/downloads/Capt_Wm_Peirce.pdf.</li>
 
 
 
