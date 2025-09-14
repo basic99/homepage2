fab3e6c0b10a687c03db655124d3120f63bad389
jim <jbwhite2@ncsu.edu>
2025-09-14 13:31:07 -0400

    genealogy page

--- article/madog_powys.php
+++ article/madog_powys.php
@@ -29,7 +29,28 @@ include "../menu.php";
 <div class="row g-5 bg-light rounded">
   <div class="col-md-2"></div>
   <div class="col-md-8">
-    <h2>Home</h2>
+    <h2>Welsh (Illegitimate) Royal Descent for Capt. William Peirce</h2>
+
+<ol>
+ <li>Madog ap Maredudd (prince of Powys). He died 1160 and had twin illegitimate sons by Efa f. Eirian - Einion Efell of Cynllaith and Cynring Efell. [1]</li>
+ <li>Einion Efell of Cynllaith, b. ca. 1130. [2]</li>
+ <li>Rhun, b. ca. 1170 = Elizabeth d. John Strange of Knockin [3]</li>
+ <li>Cuhelyn, living 1230 = Efa f. Gronwy [4]</li>
+ <li>Ieuaf ap Cuhelyn = Efa f. Adda [5]</li>
+ <li>Madog Goch, b. ca. 1270 = LLeucu f. Hywel Goch [6]</li>
+ <li>Madog Cyffin = Tangw. f. Ien Foel [7]</li>
+ <li>Ieuan Gethin ap Madog Cyffin = Margred f.Lln. [8]</li>
+ <li>Gruffid = Margred ap rhys [9]</li>
+ <li>Gwenhwyfar = Ieuan ap Gr. [10]</li>
+ <li>Dafyyd Llwyd, b. ca. 1425 [11]</li>
+ <li>Margaret vch. David b. ca. 1475, d. 1540 = John ap Llewellyn [12]</li>
+ <li>Thomasin, b. ca. 1505, d. ca. 1540 = Piers Reynold</li>
+ <li>Humfrey ap Piers Reynold, b. ca. 1535, d. 1598</li>
+ <li>Capt William Peirce, b. ca. 1580, d. ca. 1647 in Virginia.</li>
+
+
+
+</ol>
 
 
 
